bloomberg/chromium.bb

commit 32978d969ce27fa9568b30f0cdb556036a645d1e

git-cl-land: print url of final commit location
BUG=661187

Review-Url: https://codereview.chromium.org/2466953003

git_cl.py
@@ -2497,6 +2497,14 @@ class _GerritChangelistImpl(_ChangelistCodereviewBase):
       raise GerritIssueNotExists(issue, self.GetCodereviewServer())
     return data
 
+  def _GetChangeCommit(self, issue=None):
+    issue = issue or self.GetIssue()
+    assert issue, 'issue is required to query Gerrit'
+    data = gerrit_util.GetChangeCommit(self._GetGerritHost(), str(issue))
+    if not data:
+      raise GerritIssueNotExists(issue, self.GetCodereviewServer())
+    return data
+
   def CMDLand(self, force, bypass_hooks, verbose):
     if git_common.is_dirty_git_tree('land'):
       return 1
@@ -2535,6 +2543,11 @@ class _GerritChangelistImpl(_ChangelistCodereviewBase):
 
     self.SubmitIssue(wait_for_merge=True)
     print('Issue %s has been submitted.' % self.GetIssueURL())
+    links = self._GetChangeCommit().get('web_links', [])
+    for link in links:
+      if link.get('name') == 'gerrit' and link.get('url'):
+        print('Landed as %s' % link.get('url'))
+        break
     return 0
 
   def CMDPatchWithParsedIssue(self, parsed_issue_arg, reject, nocommit,

tests/git_cl_test.py
@@ -2118,11 +2118,17 @@ class TestGitCl(TestCase):
       'labels': {},
       'current_revision': 'deadbeaf',
     }
+    cl._codereview_impl._GetChangeCommit = lambda: {
+      'commit': 'deadbeef',
+      'web_links': [{'name': 'gerrit',
+                     'url': 'https://git.googlesource.com/test/+/deadbeef'}],
+    }
     cl._codereview_impl.SubmitIssue = lambda wait_for_merge: None
     out = StringIO.StringIO()
     self.mock(sys, 'stdout', out)
     self.assertEqual(0, cl.CMDLand(force=True, bypass_hooks=True, verbose=True))
     self.assertRegexpMatches(out.getvalue(), 'Issue.*123 has been submitted')
+    self.assertRegexpMatches(out.getvalue(), 'Landed as .*deadbeef')
 
   BUILDBUCKET_BUILDS_MAP = {
         '9000': {

gerrit_util.py
@@ -483,6 +483,12 @@ def GetChangeDetail(host, change, o_params=None):
   return ReadHttpJsonResponse(CreateHttpConn(host, path))
 
 
+def GetChangeCommit(host, change, revision='current'):
+  """Query a gerrit server for a revision associated with a change."""
+  path = 'changes/%s/revisions/%s/commit?links' % (change, revision)
+  return ReadHttpJsonResponse(CreateHttpConn(host, path))
+
+
 def GetChangeDescriptionFromGitiles(url, revision):
   """Query Gitiles for actual commit message for a given url and ref.
 

rubuslab/depot_tools

commit af90c602d12188fcf85533a6e07b8a9d5adc0e77

Fix gerrit/gitiles typo in git_cl
This is a fix to 32978d969ce27fa9568b30f0cdb556036a645d1e,
which introduced a "Landed as <gerrit link>" feature to git-cl, but
which at the last minute introduced a typo causing the whole feature
to not actually work.

BUG=661187

Change-Id: Ifef3379a51f035973bc5f3842862528f90bfdf84

git_cl.py
@@ -2704,7 +2704,7 @@ class _GerritChangelistImpl(_ChangelistCodereviewBase):
     print('Issue %s has been submitted.' % self.GetIssueURL())
     links = self._GetChangeCommit().get('web_links', [])
     for link in links:
-      if link.get('name') == 'gerrit' and link.get('url'):
+      if link.get('name') == 'gitiles' and link.get('url'):
         print('Landed as %s' % link.get('url'))
         break
     return 0

tests/git_cl_test.py
@@ -2750,7 +2750,7 @@ class TestGitCl(TestCase):
     }
     cl._codereview_impl._GetChangeCommit = lambda: {
       'commit': 'deadbeef',
-      'web_links': [{'name': 'gerrit',
+      'web_links': [{'name': 'gitiles',
                      'url': 'https://git.googlesource.com/test/+/deadbeef'}],
     }
     cl._codereview_impl.SubmitIssue = lambda wait_for_merge: None
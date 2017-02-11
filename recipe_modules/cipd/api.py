62977e349af309a5e57be2ff9543ac8ab105b01f
Robert Iannucci <iannucci@chromium.org>
2017-02-10 17:12:41 -0800

    [recipe_modules/cipd] Refactor creation methods.
    This clarifies what the 'create' method actually expects,
    and adds a nicer approach for recipes that want to synthesize
    their own package definition files.
    
    BUG=
    
    Change-Id: Iee5d5c02260bf71d4775ddfc1d4d1ae43937b693

--- recipe_modules/cipd/api.py
+++ recipe_modules/cipd/api.py
@@ -1,12 +1,139 @@
-# Copyright 2015 The Chromium Authors. All rights reserved.
+# Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from collections import namedtuple
+
 from recipe_engine import recipe_api
+from recipe_engine.config_types import Path
+
+
+def check_type(name, var, expect):
+  if not isinstance(var, expect):
+    raise TypeError('%s is not %s: %r (%s)' % (
+      name, type(expect).__name__, var, type(var).__name__))
+
+
+def check_list_type(name, var, expect_inner):
+  check_type(name, var, list)
+  for i, itm in enumerate(var):
+    check_type('%s[%d]' % (name, i), itm, expect_inner)
+
+
+def check_dict_type(name, var, expect_key, expect_value):
+  check_type(name, var, dict)
+  for key, value in var.iteritems():
+    check_type('%s: key' % name, key, expect_key)
+    check_type('%s[%s]' % (name, key), value, expect_value)
+
+
+class PackageDefinition(object):
+  DIR = namedtuple('DIR', ['path', 'exclusions'])
+
+  def __init__(self, package_name, package_root, install_mode):
+    """Build a new PackageDefinition.
+
+    Args:
+      package_name (str) - the name of the CIPD package
+      package_root (Path) - the path on the current filesystem that all files
+        will be relative to. e.g. if your root is /.../foo, and you add the
+        file /.../foo/bar/baz.json, the final cipd package will contain
+        'bar/baz.json'.
+      install_mode ('copy'|'symlink') - the method that cipd clients will use
+        to unpack this package locally.
+    """
+    check_type('package_name', package_name, str)
+    check_type('package_root', package_root, Path)
+    check_type('install_mode', install_mode, str)
+    if install_mode not in ('copy', 'symlink'):
+      raise ValueError('invalid value for install_mode: %r' % install_mode)
+    self.package_name = package_name
+    self.package_root = package_root
+    self.install_mode = install_mode
+
+    self.dirs = []  # list(DIR)
+    self.files = []  # list(Path)
+    self.version_file = None  # str?
+
+  def _check_in(self, path):
+    if not self.package_root.is_parent_of(path):
+      raise ValueError('path %r is not a child of the package root %r' %
+                       (path, self.package_root))
+
+  def add_dir(self, dir_path, exclusions=None):
+    """Recursively add a directory to the package.
+
+    Args:
+      dir_path (Path) - A path on the current filesystem under the
+        package_root to a directory which should be recursively included.
+      exclusions (list(str)) - A list of regexps to exclude when scanning the
+        given directory. These will be tested against the forward-slash path
+        to the file relative to `dir_path`.
+    """
+    check_type('dir_path', dir_path, Path)
+    self._check_in(dir_path)
+    exclusions = exclusions or []
+    check_list_type('exclusions', exclusions, str)
+    self.dirs.append(self.DIR(dir_path, exclusions))
+
+  def add_file(self, file_path):
+    """Add a single file to the package.
+
+    Args:
+      file_path (Path) - A path on the current filesystem to the file you
+        wish to include.
+    """
+    check_type('file_path', file_path, Path)
+    self._check_in(file_path)
+    self.files.append(file_path)
+
+  def add_version_file(self, ver_file_rel):
+    """Instruct the cipd client to place a version file in this location when
+    unpacking the package.
+
+    Version files are JSON which look like: {
+      "package_name": "infra/tools/cipd/android-amd64",
+      "instance_id": "433bfdf86c0bb82d1eee2d1a0473d3709c25d2c4"
+    }
+
+    The convention is to pick a location like '.versions/<name>.cipd_version'
+    so that a given cipd installation root might have a .versions folder full
+    of these files, one per package. This file allows executables contained
+    in the package to look for and report this file, allowing them to display
+    version information about themselves. <name> could be the name of the
+    binary tool, like 'cipd' in the example above.
+
+    A version file may be specifed exactly once per package.
+
+    Args:
+      ver_file_rel (str) - A path string relative to the installation root.
+        Should be specified in posix style (forward/slashes).
+    """
+    check_type('ver_file_rel', ver_file_rel, str)
+    if self.version_file is not None:
+      raise ValueError('add_version_file() may only be used once.')
+    self.version_file = ver_file_rel
+
+  def to_jsonish(self):
+    """Returns a JSON representation of this PackageDefinition."""
+    return {
+      'package': self.package_name,
+      'root': str(self.package_root),
+      'install_mode': self.install_mode,
+      'data': [
+        {'file': str(f)}
+        for f in self.files
+      ]+[
+        {'dir': str(d.path), 'exclude': d.exclusions}
+        for d in self.dirs
+      ]+([{'version_file': self.version_file}] if self.version_file else [])
+    }
 
 
 class CIPDApi(recipe_api.RecipeApi):
   """CIPDApi provides support for CIPD."""
+  PackageDefinition = PackageDefinition
+
   def __init__(self, *args, **kwargs):
     super(CIPDApi, self).__init__(*args, **kwargs)
     self._cipd_executable = None
@@ -63,16 +190,20 @@ class CIPDApi(recipe_api.RecipeApi):
         'cipd instance_id: %s' % step.json.output['instance_id'])
     return step
 
-  def get_executable(self):
+  @property
+  def executable(self):
+    if not self._cipd_executable:
+      raise ValueError(
+        'Your recipe must call install_client() before using'
+        ' other methods on the depot_tools/cipd module.')
     return self._cipd_executable
 
   def build(self, input_dir, output_package, package_name, install_mode=None):
-    assert self._cipd_executable
     assert not install_mode or install_mode in ['copy', 'symlink']
     return self.m.step(
         'build %s' % self.m.path.basename(package_name),
         [
-          self._cipd_executable,
+          self.executable,
           'pkg-build',
           '-in', input_dir,
           '-name', package_name,
@@ -85,10 +216,8 @@ class CIPDApi(recipe_api.RecipeApi):
     )
 
   def register(self, package_name, package_path, refs=None, tags=None):
-    assert self._cipd_executable
-
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'pkg-register', package_path,
       '-json-output', self.m.json.output(),
     ]
@@ -106,27 +235,75 @@ class CIPDApi(recipe_api.RecipeApi):
         step_test_data=lambda: self.test_api.example_register(package_name)
     )
 
-  def create(self, pkg_def, refs=None, tags=None):
-    """Creates a package based on YAML package definition file.
-
-    This builds and uploads the package in one step.
-    """
-    assert self._cipd_executable
+  def _create(self, pkg_name, pkg_def_file_or_placeholder,
+              refs=None, tags=None):
+    refs = refs or []
+    tags = tags or {}
+    check_list_type('refs', refs, str)
+    check_dict_type('tags', tags, str, str)
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'create',
-      '-pkg-def', pkg_def,
+      '-pkg-def', pkg_def_file_or_placeholder,
       '-json-output', self.m.json.output(),
     ]
     if self._cipd_credentials:
       cmd.extend(['-service-account-json', self._cipd_credentials])
-    if refs:
-      for ref in refs:
-        cmd.extend(['-ref', ref])
-    if tags:
-      for tag, value in sorted(tags.items()):
-        cmd.extend(['-tag', '%s:%s' % (tag, value)])
-    return self.m.step('create %s' % self.m.path.basename(pkg_def), cmd)
+    for ref in refs:
+      cmd.extend(['-ref', ref])
+    for tag, value in sorted(tags.items()):
+      cmd.extend(['-tag', '%s:%s' % (tag, value)])
+    result = self.m.step(
+      'create %s' % pkg_name, cmd,
+      step_test_data=lambda: self.test_api.m.json.output({
+        'result': self.test_api.make_pin(pkg_name),
+      }))
+    ret_data = result.json.output['result']
+    result.presentation.step_text = '</br>pkg: %(package)s' % ret_data
+    result.presentation.step_text += '</br>id: %(instance_id)s' % ret_data
+    return ret_data
+
+  def create_from_yaml(self, pkg_def, refs=None, tags=None):
+    """Builds and uploads a package based on on-disk YAML package definition
+    file.
+
+    This builds and uploads the package in one step.
+
+    Args:
+      pkg_def (Path) - The path to the yaml file.
+      refs (list(str)) - A list of ref names to set for the package instance.
+      tags (dict(str, str)) - A map of tag name -> value to set for the package
+                              instance.
+
+    Returns the JSON 'result' section, e.g.: {
+      "package": "infra/tools/cipd/android-amd64",
+      "instance_id": "433bfdf86c0bb82d1eee2d1a0473d3709c25d2c4"
+    }
+    """
+    check_type('pkg_def', pkg_def, Path)
+    return self._create(self.m.path.basename(pkg_def), pkg_def, refs, tags)
+
+  def create_from_pkg(self, pkg_def, refs=None, tags=None):
+    """Builds and uploads a package based on on-disk YAML package definition
+    file.
+
+    This builds and uploads the package in one step.
+
+    Args:
+      pkg_def (PackageDefinition) - The PackageDefinition object to create.
+      refs (list(str)) - A list of ref names to set for the package instance.
+      tags (dict(str, str)) - A map of tag name -> value to set for the package
+                              instance.
+
+    Returns the JSON 'result' section, e.g.: {
+      "package": "infra/tools/cipd/android-amd64",
+      "instance_id": "433bfdf86c0bb82d1eee2d1a0473d3709c25d2c4"
+    }
+    """
+    check_type('pkg_def', pkg_def, PackageDefinition)
+    return self._create(
+      pkg_def.package_name, self.m.json.input(pkg_def.to_jsonish()), refs, tags)
+
 
   def ensure(self, root, packages):
     """Ensures that packages are installed in a given root dir.
@@ -138,13 +315,11 @@ class CIPDApi(recipe_api.RecipeApi):
     If installing a package requires credentials, call
     ``set_service_account_credentials`` before calling this function.
     """
-    assert self._cipd_executable
-
     package_list = ['%s %s' % (name, version)
                     for name, version in sorted(packages.items())]
     ensure_file = self.m.raw_io.input('\n'.join(package_list))
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'ensure',
       '-root', root,
       '-ensure-file', ensure_file,
@@ -158,10 +333,8 @@ class CIPDApi(recipe_api.RecipeApi):
     )
 
   def set_tag(self, package_name, version, tags):
-    assert self._cipd_executable
-
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'set-tag', package_name,
       '-version', version,
       '-json-output', self.m.json.output(),
@@ -180,10 +353,8 @@ class CIPDApi(recipe_api.RecipeApi):
     )
 
   def set_ref(self, package_name, version, refs):
-    assert self._cipd_executable
-
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'set-ref', package_name,
       '-version', version,
       '-json-output', self.m.json.output(),
@@ -202,11 +373,10 @@ class CIPDApi(recipe_api.RecipeApi):
     )
 
   def search(self, package_name, tag):
-    assert self._cipd_executable
     assert ':' in tag, 'tag must be in a form "k:v"'
 
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'search', package_name,
       '-tag', tag,
       '-json-output', self.m.json.output(),
@@ -222,10 +392,8 @@ class CIPDApi(recipe_api.RecipeApi):
 
   def describe(self, package_name, version,
                test_data_refs=None, test_data_tags=None):
-    assert self._cipd_executable
-
     cmd = [
-      self._cipd_executable,
+      self.executable,
       'describe', package_name,
       '-version', version,
       '-json-output', self.m.json.output(),

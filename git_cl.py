eefe07abfe950760535f5b90893d8c5f1fca8dfc
Andrii Shyshkalov <tandrii@chromium.org>
2017-03-15 21:07:36 +0100

    git cl description: don't bother if description diff is trailing whitespace.
    BUG=700239
    R=iannucci@chromium.org
    
    Change-Id: Ica4567288c1cd83f72cce5d10c523270b69c36d4

--- git_cl.py
+++ git_cl.py
@@ -4159,7 +4159,7 @@ def CMDdescription(parser, args):
   else:
     description.prompt()
 
-  if cl.GetDescription() != description.description:
+  if cl.GetDescription().strip() != description.description:
     cl.UpdateDescription(description.description, force=options.force)
   return 0
 

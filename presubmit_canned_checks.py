522325f4cc0cecfeff42b04b4deea0efd313a679
anatoly techtonik <techtonik@gmail.com>
2017-03-28 22:37:41 +0300

    Fix pylint file list matching on Windows
    Windows paths use backward slash separator, which
    escapes regular expressions for white and black lists
    in PRESUBMIT.py filters
    
    See https://goo.gl/6c7aku for discussion
    
    Bug:
    
    Change-Id: Ia1d133c5b6cfa6cb364b1500b7b7abe583bce346

--- presubmit_canned_checks.py
+++ presubmit_canned_checks.py
@@ -525,7 +525,8 @@ def GetUnitTestsInDirectory(
       continue
     unit_tests.append(input_api.os_path.join(directory, filename))
     to_run += 1
-  input_api.logging.debug('Found %d files, running %d' % (found, to_run))
+  input_api.logging.debug('Found %d files, running %d unit tests'
+                          % (found, to_run))
   if not to_run:
     return [
         output_api.PresubmitPromptWarning(
@@ -675,6 +676,14 @@ def _FetchAllFiles(input_api, white_list, black_list):
   # can break another unmodified file.
   # Use code similar to InputApi.FilterSourceFile()
   def Find(filepath, filters):
+    if input_api.platform == 'win32':
+      # Transform *relative* path to posix for proper regex matching.
+      # This will hang if filepath starts with slash.
+      newpath = [filepath]
+      while input_api.os_path.dirname(newpath[0]):
+        newpath[0:1] = input_api.os_path.split(newpath[0])
+      filepath = '/'.join(newpath)
+
     for item in filters:
       if input_api.re.match(item, filepath):
         return True

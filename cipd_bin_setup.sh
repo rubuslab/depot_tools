4296a29dd10cac2e6b1638255bb219783c24a51e
Sergey Berezin <sergeyberezin@google.com>
2017-05-01 15:25:19 -0700

    Add vpython to depot_tools for linux and mac
    BUG=717208
    R=dnj@chromium.org
    
    Change-Id: If937a382be9aa4d8eb5f957386e8b1b28cc1c3ac

---
+++ cipd_bin_setup.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+MYPATH=$(dirname "${BASH_SOURCE[0]}")
+
+"$MYPATH/cipd" ensure -ensure-file "$MYPATH/cipd_manifest.txt" -root "$MYPATH/.cipd_bin"

2fadca92f7fd6cbd6059166b73dec3987abf87a3
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-05-22 19:10:40 +0200

    gclient: enable syntax validation by default
    Please pass --disable-validate-syntax as an escape hatch.
    
    Bug: 570091
    Change-Id: Iceadc0d2fc54ca68e775c3bc7590f4329ddb4951

--- gclient.py
+++ gclient.py
@@ -2054,8 +2054,11 @@ def CMDsync(parser, args):
                     help='DEPRECATED: This is a no-op.')
   parser.add_option('-m', '--manually_grab_svn_rev', action='store_true',
                     help='DEPRECATED: This is a no-op.')
-  parser.add_option('--validate-syntax', action='store_true',
+  parser.add_option('--validate-syntax', action='store_true', default=True,
                     help='Validate the .gclient and DEPS syntax')
+  parser.add_option('--disable-validate-syntax', action='store_false',
+                    dest='validate_syntax',
+                    help='Disable validation of .gclient and DEPS syntax.')
   (options, args) = parser.parse_args(args)
   client = GClient.LoadCurrentConfig(options)
 

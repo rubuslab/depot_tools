b10b400957df292d6c07a8c7dd36aa07bf999ea1
Dan Jacques <dnj@google.com>
2017-07-06 20:15:04 -0700

    [win_tools] Use bundled Python CIPD packages.
    Enable bundled Python CIPD packages in bleeding-edge mode. This
    replaces the ZIP unpacking approach used before, and introduces
    validation and management through the CIPD tool. The bleeding edge
    version will only install if a sentinel file is present in the
    "depot_tools" root; otherwise, default behavior will continue.
    
    The CIPD Python package is installed by examining a manifest file. If
    the manifest's contents differ from a stamp, the CIPD command is
    executed to ensure the new version.
    
    The new Python should have the same facilities as the original Python
    bundle.
    
    BUG=chromium:740171
    TEST=local
    
    Change-Id: I1b3b7d31d47d1a37a9dba9114d31681bec558736

--- bootstrap/win/git-bash.template.sh
+++ bootstrap/win/git-bash.template.sh
@@ -2,7 +2,7 @@
 export EDITOR=${EDITOR:=notepad}
 WIN_BASE=`dirname $0`
 UNIX_BASE=`cygpath "$WIN_BASE"`
-export PATH=$PATH:$UNIX_BASE/SVN_BIN_DIR:$UNIX_BASE/PYTHON_BIN_DIR:$UNIX_BASE/PYTHON_BIN_DIR/Scripts
+export PATH=$PATH:$UNIX_BASE/SVN_BIN_DIR:$UNIX_BASE/python27_bin/bin:$UNIX_BASE/python27_bin/bin/Scripts
 export PYTHON_DIRECT=1
 export PYTHONUNBUFFERED=1
 if [[ $# > 0 ]]; then

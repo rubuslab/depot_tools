b636eb63ed602aab8b382d0150f862877f2f147d
Dan Jacques <dnj@google.com>
2017-07-07 19:10:19 -0700

    [win_tools] Use bundled Python CIPD packages.
    Enable bundled Python CIPD packages in bleeding-edge mode. This
    replaces the ZIP unpacking approach used before, and introduces
    validation and management through the CIPD tool. The bleeding edge
    version will only install if a sentinel file is present in the
    "depot_tools" root; otherwise, default behavior will continue.
    
    This method adds a upgrade and downgrade path to/from ZIP and
    CIPD installations. This is done by rewriting the "win_tools.bat"
    process:
    
    1) Ensure that a bootstrap Python is present.
    2) Use it to run "win_tools.py", which has the functionality of
        "git_bootstrap.py" plus Python installation.
    3) Run "win_tools.py" with appropriate flags.
    
    Some tricks were employed to handle cases where there is an
    already-running Python instance that uses the current Python
    installation and executable. This happens on bots because the
    system uses the same "depot_tools" checkout at multiple launch
    layers. To this end, we use the "python.bat" as the "current Python"
    authority and refrain from cleaning up old Python directories if their
    "python.exe" binaries are currently in use.
    
    We change the Git bleeding edge file to share the same
    sentinel file as Python, ".bleeding_edge".
    
    The new Python should have the same facilities as the original Python
    bundle.
    
    BUG=chromium:740171
    TEST=local
    
    Change-Id: I1b3b7d31d47d1a37a9dba9114d31681bec558736

--- bootstrap/win/win_tools.bat
+++ bootstrap/win/win_tools.bat
@@ -1,69 +1,175 @@
-@echo off
+::@echo off
 :: Copyright (c) 2012 The Chromium Authors. All rights reserved.
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
 :: This script will determine if python or git binaries need updates. It
-:: returns 123 if the user's shell must restart, otherwise !0 is failure
+:: returns !0 as failure
+::
+:: A previous Python may actually be in use when it is run, preventing us
+:: from replacing it outright without breaking running code. To
+:: accommodate this, and Python cleanup, we handle Python in two stages:
+:: 1) Ensure that *some* Python is available on the system.
+:: 2) Run a follow-up program to complete installation and clean up any
+::    unused Pythons.
+::
+:: We detect if there is an existing Python by looking for a "python.bat"
+:: shim. If it is absent, we will directly install the non-bleeding-edge
+:: Python instance.
+::
+:: The post-processing will regenerate "python.bat" to point to the current
+:: Python instance. Any previous Python installations will stick around, but
+:: new invocations will use the new instance. Old installations will die
+:: off either due to processes terminating or systems restarting. When this
+:: happens, they will be cleaned up by the post-processing script.
+::
+:: When modifying this script, run the following test scenario. After each
+:: installation, re-run this script to ensure that it is a no-op.
+:: - Cold (git clean -x -f -d .) installation.
+::   - Run "python.bat" in another shell, keep it open, add ".bleeding_edge"
+::     and test warm downgrade.
+::   - Close the Python shell, run again, ensure that old directory is cleaned.
+:: - Cold w/ ".bleeding_edge" installation.
+::   - Run "python.bat" in another shell, keep it open, delete ".bleeding_edge"
+::     and test warm downgrade.
+::   - Close the Python shell, run again, ensure that old directory is cleaned.
+:: - Run "git bash", ensure that the shell works.
+::   - Run "python" in the shell, ensure that an interpreter opens.
+::   - Run "git map-branches" in the shell, ensure that this works.
 
-:: Sadly, we can't use SETLOCAL here otherwise it ERRORLEVEL is not correctly
-:: returned.
+:: Note: we set EnableDelayedExpansion so we can perform string manipulations
+:: in our manifest parsing loop. This only works on Windows XP+.
+setlocal EnableDelayedExpansion
 
 set CHROME_INFRA_URL=https://storage.googleapis.com/chrome-infra/
 :: It used to be %~dp0 but ADODB.Stream may fail to write to this directory if
 :: the directory DACL is set to elevated integrity level.
-set ZIP_DIR=%TEMP%
+set STAGING_DIR=%TEMP%
 
 :: Get absolute root directory (.js scripts don't handle relative paths well).
 pushd %~dp0..\..
 set WIN_TOOLS_ROOT_DIR=%CD%
 popd
 
+:: Extra arguments to pass to our "win_tools.py" script.
+set WIN_TOOLS_EXTRA_ARGS=--verbose
+set WIN_TOOLS_PYTHON_BIN=%WIN_TOOLS_ROOT_DIR%\python.bat
+
+:: TODO: Deprecate this when legacy mode is disabled.
 if "%1" == "force" (
-  set WIN_TOOLS_FORCE=1
+  set WIN_TOOLS_EXTRA_ARGS=%WIN_TOOLS_EXTRA_ARGS% --force
   shift /1
 )
 
+:: Determine if we're running a bleeding-edge installation.
+set BOOTSTRAP_ROOT=%STAGING_DIR%\python27_bootstrap_bin
+if not exist "%WIN_TOOLS_ROOT_DIR%\.bleeding_edge" (
+  set CIPD_MANIFEST=
+) else (
+  set CIPD_MANIFEST=manifest_bleeding_edge.txt
+  set WIN_TOOLS_EXTRA_ARGS=%WIN_TOOLS_EXTRA_ARGS% --bleeding-edge
+)
 
-:PYTHON_CHECK
-if not exist "%WIN_TOOLS_ROOT_DIR%\python276_bin" goto :PY27_INSTALL
-if not exist "%WIN_TOOLS_ROOT_DIR%\python.bat" goto :PY27_INSTALL
-set ERRORLEVEL=0
-goto :GIT_CHECK
+:: Identify our CIPD executable. If the client executable exists, use it
+:: directly; otherwise, use "cipd.bat" to bootstrap the client. This
+:: optimization is useful because this script can be run frequently, and
+:: reduces execution time noticeably.
+::
+:: See "//cipd.bat" and "//cipd.ps1" for more information.
+set CIPD_EXE=%WIN_TOOLS_ROOT_DIR%\.cipd_client.exe
+if not exist "%CIPD_EXE%" set CIPD_EXE=%WIN_TOOLS_ROOT_DIR%\cipd.bat
+set WIN_TOOLS_EXTRA_ARGS=%WIN_TOOLS_EXTRA_ARGS% --cipd-client "%CIPD_EXE%"
+
+:: TODO: This logic will change when we deprecate legacy mode. For now, we
+:: assume !bleeding_edge == legacy.
+if "%CIPD_MANIFEST%" == "" goto :PY27_LEGACY_CHECK
+
+:: We are committed to CIPD, and will use "win_tools.py" to perform our
+:: finalization.
+::
+:: Parse our CIPD manifest and identify the "cpython" version. We do this by
+:: reading it line-by-line, identifying the line containing "cpython", and
+:: stripping all text preceding "versison:". This leaves us with the version
+:: string.
+::
+:: This method requires EnableDelayedExpansion, and extracts the Python version
+:: from our CIPD manifest. Variables referenced using "!" instead of "%" are
+:: delayed expansion variables.
+for /F "tokens=*" %%A in (%~dp0%CIPD_MANIFEST%) do (
+  set LINE=%%A
+  if not "x!LINE:cpython=!" == "x!LINE!" set PYTHON_VERSION=!LINE:*version:=!
+)
+if "%PYTHON_VERSION%" == "" (
+  @echo Could not extract Python version from manifest.
+  set ERRORLEVEL=1
+  goto :END
+)
+
+:: We will take the version string, replace "." with "_", and surround it with
+:: "win-tools-<PYTHON_VERSION>_bin" so that it matches "win_tools.py"'s cleanup
+:: expression and ".gitignore".
+::
+:: We incorporate PYTHON_VERSION into the "win_tools" directory name so that
+:: new installations don't interfere with long-running Python processes if
+:: Python is upgraded.
+set WIN_TOOLS_NAME=win_tools-%PYTHON_VERSION:.=_%_bin
+set WIN_TOOLS_PATH=%WIN_TOOLS_ROOT_DIR%\%WIN_TOOLS_NAME%
+set WIN_TOOLS_EXTRA_ARGS=%WIN_TOOLS_EXTRA_ARGS% --win-tools-name "%WIN_TOOLS_NAME%"
+
+:: Install our CIPD packages.
+call "%CIPD_EXE%" ensure -ensure-file "%~dp0%CIPD_MANIFEST%" -root "%WIN_TOOLS_PATH%"
+if errorlevel 1 goto :END
+
+set WIN_TOOLS_PYTHON_BIN=%WIN_TOOLS_PATH%\python\bin\python.exe
+goto :WIN_TOOLS_PY
 
 
-:PY27_INSTALL
+:: LEGACY Support
+::
+:: This is a full Python installer. It falls through to "win_tools.py",
+:: instructing it to not handle Python installation. This should be removed
+:: once we commit to CIPD.
+:PY27_LEGACY_CHECK
+if not exist "%WIN_TOOLS_ROOT_DIR%\python.bat" goto :PY27_LEGACY_INSTALL
+if not exist "%WIN_TOOLS_ROOT_DIR%\python276_bin" goto :PY27_LEGACY_INSTALL
+goto :WIN_TOOLS_PY
+
+
+:PY27_LEGACY_INSTALL
 echo Installing python 2.7.6...
 :: Cleanup python directory if it was existing.
 set PYTHON_URL=%CHROME_INFRA_URL%python276_bin.zip
 if exist "%WIN_TOOLS_ROOT_DIR%\python276_bin\." rd /q /s "%WIN_TOOLS_ROOT_DIR%\python276_bin"
-if exist "%ZIP_DIR%\python276.zip" del "%ZIP_DIR%\python276.zip"
+if exist "%STAGING_DIR%\python276.zip" del "%STAGING_DIR%\python276.zip"
 echo Fetching from %PYTHON_URL%
-cscript //nologo //e:jscript "%~dp0get_file.js" %PYTHON_URL% "%ZIP_DIR%\python276_bin.zip"
-if errorlevel 1 goto :PYTHON_FAIL
+cscript //nologo //e:jscript "%~dp0get_file.js" %PYTHON_URL% "%STAGING_DIR%\python276_bin.zip"
+if errorlevel 1 goto :PYTHON_LEGACY_FAIL
 :: Will create python276_bin\...
-cscript //nologo //e:jscript "%~dp0unzip.js" "%ZIP_DIR%\python276_bin.zip" "%WIN_TOOLS_ROOT_DIR%"
+cscript //nologo //e:jscript "%~dp0unzip.js" "%STAGING_DIR%\python276_bin.zip" "%WIN_TOOLS_ROOT_DIR%"
 :: Create the batch files.
 call copy /y "%~dp0python276.new.bat" "%WIN_TOOLS_ROOT_DIR%\python.bat" 1>nul
 call copy /y "%~dp0pylint.new.bat" "%WIN_TOOLS_ROOT_DIR%\pylint.bat" 1>nul
 del "%ZIP_DIR%\python276_bin.zip"
 set ERRORLEVEL=0
-goto :GIT_CHECK
+goto :WIN_TOOLS_PY
 
 
-:PYTHON_FAIL
+:PYTHON_LEGACY_FAIL
 echo ... Failed to checkout python automatically.
 echo You should get the "prebaked" version at %PYTHON_URL%
 set ERRORLEVEL=1
 goto :END
 
-:GIT_CHECK
-"%WIN_TOOLS_ROOT_DIR%\python.bat" "%~dp0git_bootstrap.py"
-goto :END
 
-:returncode
-set WIN_TOOLS_ROOT_DIR=
-exit /b %ERRORLEVEL%
+:: This executes "win_tools.py" using the WIN_TOOLS_PYTHON_BIN Python
+:: interpreter.
+:WIN_TOOLS_PY
+call "%WIN_TOOLS_PYTHON_BIN%" "%~dp0win_tools.py" %WIN_TOOLS_EXTRA_ARGS%
+
 
 :END
-call :returncode %ERRORLEVEL%
+set EXPORT_ERRORLEVEL=%ERRORLEVEL%
+endlocal & (
+  set ERRORLEVEL=%EXPORT_ERRORLEVEL%
+)
+exit /b %ERRORLEVEL%

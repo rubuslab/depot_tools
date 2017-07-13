63924a123a19fd3038255eefe264365cfdb31d10
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-07-13 13:16:55 +0200

    depot_tools test recipe CL
    (testing commit message change)
    
    Bug:
    Change-Id: Ib8f6c8091caa6e404986a00b1897303ca3e10db8
    Recipe-Nontrivial-Roll: infra

--- recipes/recipe_modules/depot_tools/api.py
+++ recipes/recipe_modules/depot_tools/api.py
@@ -5,9 +5,9 @@
 from recipe_engine import recipe_api
 
 class DepotToolsApi(recipe_api.RecipeApi):
-  @property
-  def download_from_google_storage_path(self):
-    return self.package_repo_resource('download_from_google_storage.py')
+  #@property
+  #def download_from_google_storage_path(self):
+  #  return self.package_repo_resource('download_from_google_storage.py')
 
   @property
   def upload_to_google_storage_path(self):

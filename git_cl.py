aa4f334018d3d7f1b0e72837cd0aaf92e6f38af3
Stefan Zager <szager@chromium.org>
2017-10-10 15:11:57 -0700

    Add --issue option to 'git cl try-results'
    Sometimes I want to get try bot results without checking out the
    relevant branch.
    
    R=agable@chromium.org
    
    Bug:
    Change-Id: I89d118962ee37b10d95cf4044b61a0f2234b7e60

--- git_cl.py
+++ git_cl.py
@@ -2782,7 +2782,6 @@ class _GerritChangelistImpl(_ChangelistCodereviewBase):
   def CMDPatchWithParsedIssue(self, parsed_issue_arg, reject, nocommit,
                               directory, force):
     assert not reject
-    assert not nocommit
     assert not directory
     assert parsed_issue_arg.valid
 
@@ -2817,6 +2816,9 @@ class _GerritChangelistImpl(_ChangelistCodereviewBase):
       RunGit(['reset', '--hard', 'FETCH_HEAD'])
       print('Checked out commit for change %i patchset %i locally' %
             (parsed_issue_arg.issue, patchset))
+    elif nocommit:
+      RunGit(['cherry-pick', '--no-commit', 'FETCH_HEAD'])
+      print('Patch applied to index.')
     else:
       RunGit(['cherry-pick', 'FETCH_HEAD'])
       print('Committed patch for change %i patchset %i locally.' %
@@ -5408,8 +5410,6 @@ def CMDpatch(parser, args):
   if cl.IsGerrit():
     if options.reject:
       parser.error('--reject is not supported with Gerrit codereview.')
-    if options.nocommit:
-      parser.error('--nocommit is not supported with Gerrit codereview.')
     if options.directory:
       parser.error('--directory is not supported with Gerrit codereview.')
 
@@ -5590,12 +5590,16 @@ def CMDtry_results(parser, args):
                       'or "-" for stdout.'))
   parser.add_option_group(group)
   auth.add_auth_options(parser)
+  _add_codereview_issue_select_options(parser)
   options, args = parser.parse_args(args)
+  _process_codereview_issue_select_options(parser, options)
   if args:
     parser.error('Unrecognized args: %s' % ' '.join(args))
 
   auth_config = auth.extract_auth_config_from_options(options)
-  cl = Changelist(auth_config=auth_config)
+  cl = Changelist(
+      issue=options.issue, codereview=options.forced_codereview,
+      auth_config=auth_config)
   if not cl.GetIssue():
     parser.error('Need to upload first')
 

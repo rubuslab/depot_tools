bloomberg/chromium.bb

commit 209a681f9cb0e955d5659216f262ef5b00861e8a

[git] Remove hard-coded "depot_tools" Git.
Several tools, including the "git" recipe module, hard-code a
checkout-relative "git.bat" path. Git is a feature that is provided by
the system, both to tooling and recipes:

1) For users, "depot_tools" must be on PATH, and during setup it will
   have installed "git.bat", ensuring that Git tooling is available in
   PATH.
2) For bots, the system is responsible for providing "git.bat" on PATH.
   This is typically done at "/b/depot_tools/git.bat", which is sync'd
   through the "update_scripts" step.

By formally treating Git as a system resource, we absolve Windows bots
and users from manually installing a depot_tools-local Git, bringing
them in line with other platforms.

BUG=chromium:590806
TEST=local

Change-Id: I93e89855cdd330a2ba7a8cfb8117a1789d1ab54e
Reviewed-on: https://chromium-review.googlesource.com/568694
Commit-Queue: Daniel Jacques <dnj@chromium.org>
Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

fetch.py
@@ -26,6 +26,8 @@ import subprocess
 import sys
 import textwrap
 
+import git_common
+
 from distutils import spawn
 
 
@@ -88,11 +90,8 @@ class GclientCheckout(Checkout):
 class GitCheckout(Checkout):
 
   def run_git(self, *cmd, **kwargs):
-    if sys.platform == 'win32' and not spawn.find_executable('git'):
-      git_path = os.path.join(SCRIPT_PATH, 'git.bat')
-    else:
-      git_path = 'git'
-    return self.run((git_path,) + cmd, **kwargs)
+    print 'Running: git %s' % (' '.join(pipes.quote(x) for x in cmd))
+    return git_common.run(*cmd, **kwargs)
 
 
 class GclientGitCheckout(GclientCheckout, GitCheckout):

git_common.py
@@ -37,8 +37,20 @@ from StringIO import StringIO
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 IS_WIN = sys.platform == 'win32'
-GIT_EXE = ROOT+'\\git.bat' if IS_WIN else 'git'
 TEST_MODE = False
+
+
+def win_find_git():
+  for elem in os.environ.get('PATH', '').split(os.pathsep):
+    for candidate in ('git.exe', 'git.bat'):
+      path = os.path.join(elem, candidate)
+      if os.path.isfile(path):
+        return path
+  raise ValueError('Could not find Git on PATH.')
+
+
+GIT_EXE = 'git' if not IS_WIN else win_find_git()
+
 
 FREEZE = 'FREEZE'
 FREEZE_SECTIONS = {

recipes/recipe_modules/git/api.py
@@ -10,18 +10,11 @@ from recipe_engine import recipe_api
 class GitApi(recipe_api.RecipeApi):
   _GIT_HASH_RE = re.compile('[0-9a-f]{40}', re.IGNORECASE)
 
-  def __init__(self, *args, **kwargs):
-    super(GitApi, self).__init__(*args, **kwargs)
-    self.initialized_win_git = False
-
   def __call__(self, *args, **kwargs):
     """Return a git command step."""
     name = kwargs.pop('name', 'git ' + args[0])
     infra_step = kwargs.pop('infra_step', True)
     git_cmd = ['git']
-    if self.m.platform.is_win:
-      self.ensure_win_git_tooling()
-      git_cmd = [self.package_repo_resource('git.bat')]
     options = kwargs.pop('git_config_options', {})
     for k, v in sorted(options.iteritems()):
       git_cmd.extend(['-c', '%s=%s' % (k, v)])
@@ -35,19 +28,6 @@ class GitApi(recipe_api.RecipeApi):
         raise
       else:
         return f.result
-
-  def ensure_win_git_tooling(self):
-    """Ensures that depot_tools/git.bat actually exists."""
-    if not self.m.platform.is_win or self.initialized_win_git:
-      return
-    with self.m.context(cwd=self.package_repo_resource()):
-      self.m.python(
-          'ensure git tooling on windows',
-          self.package_repo_resource('bootstrap', 'win', 'git_bootstrap.py'),
-          ['--verbose'],
-          infra_step=True,
-          timeout=300)
-    self.initialized_win_git = True
 
   def fetch_tags(self, remote_name=None, **kwargs):
     """Fetches all tags from the remote."""
@@ -196,11 +176,6 @@ class GitApi(recipe_api.RecipeApi):
       git_setup_args += ['--remote', remote_name]
     else:
       remote_name = 'origin'
-
-    if self.m.platform.is_win:
-      self.ensure_win_git_tooling()
-      git_setup_args += [
-          '--git_cmd_path', self.package_repo_resource('git.bat')]
 
     step_suffix = '' if step_suffix is  None else ' (%s)' % step_suffix
     self.m.python(

recipes/recipe_modules/git/resources/git_setup.py
@@ -11,18 +11,17 @@ import os
 import subprocess
 import sys
 
+# Import "git_common" from "depot_tools" root.
+DEPOT_TOOLS_ROOT = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, os.pardir))
+sys.path.insert(0, DEPOT_TOOLS_ROOT)
+import git_common
 
-def run_git(git_cmd, *args, **kwargs):
-  """Runs git with given arguments.
 
-  kwargs are passed through to subprocess.
-
-  If the kwarg 'throw' is provided, this behaves as check_call, otherwise will
-  return git's return value.
-  """
-  logging.info('Running: %s %s %s', git_cmd, args, kwargs)
-  func = subprocess.check_call if kwargs.pop('throw', True) else subprocess.call
-  return func((git_cmd,)+args, **kwargs)
+def run_git(*cmd, **kwargs):
+  kwargs['stdout'] = sys.stdout
+  kwargs['stderr'] = sys.stderr
+  git_common.run(*cmd, **kwargs)
 
 
 def main():
@@ -31,9 +30,6 @@ def main():
                       required=True)
   parser.add_argument('--url', help='URL of remote to make origin.',
                       required=True)
-  parser.add_argument('--git_cmd_path',
-                      help='Path to the git command to run.',
-                      default='git')
   parser.add_argument('--remote', help='Name of the git remote.',
                       default='origin')
   parser.add_argument('-v', '--verbose', action='store_true')
@@ -49,11 +45,10 @@ def main():
     os.makedirs(path)
 
   if os.path.exists(os.path.join(path, '.git')):
-    run_git(opts.git_cmd_path, 'config', '--remove-section',
-            'remote.%s' % remote, cwd=path)
+    run_git('config', '--remove-section', 'remote.%s' % remote, cwd=path)
   else:
-    run_git(opts.git_cmd_path, 'init', cwd=path)
-  run_git(opts.git_cmd_path, 'remote', 'add', remote, url, cwd=path)
+    run_git('init', cwd=path)
+  run_git('remote', 'add', remote, url, cwd=path)
   return 0
 
 

rubuslab/depot_tools

commit df8ee008c1ba8443ac6cb2ab7e1bd84890ac29cf

Fix fetch --dry-run to not invoke git
This was broken by 209a681f9cb0e955d5659216f262ef5b00861e8a,
which replaced a self.run() with git_common.run(), thus
bypassing self.run()'s dry_run check.

Bug: 788562
Change-Id: I9964c169b0ff56778c4660acab7ef7c010e37a36

fetch.py
@@ -91,6 +91,8 @@ class GitCheckout(Checkout):
 
   def run_git(self, *cmd, **kwargs):
     print 'Running: git %s' % (' '.join(pipes.quote(x) for x in cmd))
+    if self.options.dry_run:
+      return ''
     return git_common.run(*cmd, **kwargs)
 
 
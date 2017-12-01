b0a0000aae8718904f2c1b7a7b470e918655ccc7
Marc-Antoine Ruel <maruel@chromium.org>
2017-11-30 20:25:59 -0500

    Add more tests for fix_encoding.py
    In practice, there's something magical that happens when a python call another
    process, even if there's a bash in between, that causse the child process to not
    be affect by the lack of LANG variable. This is disconcerting, as if I do:
      export -n LANG
      ./tests/fix_encoding_test.py
    
    FixEncodingTest.test_unicode fails with:
      Traceback (most recent call last):
        File "./tests/fix_encoding_test.py", line 45, in test_unicode
            print self.text
            UnicodeEncodeError: 'ascii' codec can't encode character u'\xe9' in
            position 1: ordinal not in range(128)
    
    Yet all the tests print out \xff without problem. The only difference is if
    stdout/stderr is piped. For example, the following pass:
      export -n LANG
      ./tests/fix_encoding_test.py &> a.txt
      cat a.txt
    
    I don't see a way to get around that. The unit tests are still valuable so
    committing them.
    
    Test only change.
    
    R=vadimsh@chromium.org
    Bug: 787531
    Change-Id: I0c97ba7b8625b06017135f48363c55dd4fc40557

--- tests/fix_encoding_test.py
+++ tests/fix_encoding_test.py
@@ -7,17 +7,28 @@
 """Unit tests for fix_encoding.py."""
 
 import os
+import subprocess
 import sys
+import tempfile
 import unittest
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, ROOT_DIR)
 
 import fix_encoding
 
 
+# \U0001f310  U+1F310  utf-8: \xf0\x9f\x8c\x90
+PRINT_EMOJI = (
+  'import fix_encoding,sys;print fix_encoding.fix_encoding();'
+  'print "\\xf0\\x9f\\x8c\\x90".decode("utf-8");'
+  'print "\xff"'
+  )
+
 class FixEncodingTest(unittest.TestCase):
   # Nice mix of latin, hebrew, arabic and chinese. Doesn't mean anything.
-  text = u'Héllô 偉大 سيد'
+  text = u'Héllô 偉大 سيد🌐'
 
   def test_code_page(self):
     # Make sure printing garbage won't throw.
@@ -53,6 +64,81 @@ class FixEncodingTest(unittest.TestCase):
   def test_multiple_calls(self):
     # Shouldn't do anything.
     self.assertEquals(False, fix_encoding.fix_encoding())
+
+  def _run(self, cmd, env):
+    out = subprocess.check_output(cmd, cwd=ROOT_DIR, env=env)
+    self.assertEqual(u'True\n\U0001f310\n'.encode('utf-8') + '\xff\n', out)
+    self.assertEqual(u'True\n🌐\n'.encode('utf-8') + '\xff\n', out)
+
+  def _run_as_arg(self, env):
+    # When testing on the infrastructure, the current directory may not be in
+    # PYTHONPATH.
+    env['PYTHONPATH'] = os.pathsep.join((ROOT_DIR, env.get('PYTHONPATH') or ''))
+    self._run([sys.executable, '-c', PRINT_EMOJI], env)
+
+  def _run_tmp_file(self, env):
+    h, tmp = tempfile.mkstemp(prefix='fix_encoding_test', suffix='.py')
+    os.write(h, '# coding=utf-8\n')
+    os.write(h, PRINT_EMOJI + '\n')
+    os.close(h)
+    try:
+      self._run([sys.executable, tmp], env)
+    finally:
+      os.remove(tmp)
+
+  def test_lang_default(self):
+    self._run_as_arg(os.environ.copy())
+
+  def test_lang_unset(self):
+    env = os.environ.copy()
+    env.pop('LANG', None)
+    env.pop('LANGUAGE', None)
+    self._run_as_arg(env)
+
+  def test_lang_empty(self):
+    env = os.environ.copy()
+    env['LANG'] = ''
+    env['LANGUAGE'] = ''
+    self._run_as_arg(env)
+
+  def test_lang_C(self):
+    env = os.environ.copy()
+    env['LANG'] = 'C.UTF-8'
+    env['LANGUAGE'] = 'C.UTF-8'
+    self._run_as_arg(env)
+
+  def test_lang_bad(self):
+    env = os.environ.copy()
+    env['LANG'] = 'C.ASCII'
+    env['LANGUAGE'] = 'C.ASCII'
+    self._run_as_arg(env)
+
+  def test_script_lang_default(self):
+    self._run_tmp_file(os.environ.copy())
+
+  def test_script_lang_unset(self):
+    env = os.environ.copy()
+    env.pop('LANG', None)
+    env.pop('LANGUAGE', None)
+    self._run_tmp_file(env)
+
+  def test_script_lang_empty(self):
+    env = os.environ.copy()
+    env['LANG'] = ''
+    env['LANGUAGE'] = ''
+    self._run_tmp_file(env)
+
+  def test_script_lang_C(self):
+    env = os.environ.copy()
+    env['LANG'] = 'C.UTF-8'
+    env['LANGUAGE'] = 'C.UTF-8'
+    self._run_tmp_file(env)
+
+  def test_script_lang_bad(self):
+    env = os.environ.copy()
+    env['LANG'] = 'C.ASCII'
+    env['LANGUAGE'] = 'C.ASCII'
+    self._run_tmp_file(env)
 
 
 if __name__ == '__main__':

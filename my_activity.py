c7ab0ccd15ee38e3794bdbc8a25732ed75e79241
Sergiy Byelozyorov <sergiyb@chromium.org>
2018-03-16 22:19:39 +0100

    Add progress indicator that prints a dot on nearly each RPCs request
    R=tandrii@chromium.org
    
    Change-Id: Id4fa494c8225c8edefecba064445504065581478

--- my_activity.py
+++ my_activity.py
@@ -199,6 +199,11 @@ class MyActivity(object):
     self.check_cookies()
     self.google_code_auth_token = None
 
+  def progress(self):
+    if self.options.progress:
+      sys.stdout.write('.')
+      sys.stdout.flush()
+
   # Check the codereview cookie jar to determine which Rietveld instances to
   # authenticate to.
   def check_cookies(self):
@@ -247,6 +252,7 @@ class MyActivity(object):
         reviewer=reviewer_email,
         modified_after=query_modified_after,
         with_messages=True)
+    self.progress()
 
     issues = filter(
         lambda i: (datetime_from_rietveld(i['created']) < self.modified_before),
@@ -296,6 +302,7 @@ class MyActivity(object):
       patchset_props = remote.get_patchset_properties(
           issue['issue'],
           issue['patchsets'][-1])
+      self.progress()
       ret['delta'] = '+%d,-%d' % (
           sum(f['num_added'] for f in patchset_props['files'].itervalues()),
           sum(f['num_removed'] for f in patchset_props['files'].itervalues()))
@@ -346,16 +353,17 @@ class MyActivity(object):
       ret.append(r)
     return ret
 
-  @staticmethod
-  def gerrit_changes_over_rest(instance, filters):
+  def gerrit_changes_over_rest(self, instance, filters):
     # Convert the "key:value" filter to a list of (key, value) pairs.
     req = list(f.split(':', 1) for f in filters)
     try:
       # Instantiate the generator to force all the requests now and catch the
       # errors here.
-      return list(gerrit_util.GenerateAllChanges(instance['url'], req,
+      changes = list(gerrit_util.GenerateAllChanges(instance['url'], req,
           o_params=['MESSAGES', 'LABELS', 'DETAILED_ACCOUNTS',
                     'CURRENT_REVISION', 'CURRENT_COMMIT']))
+      self.progress()
+      return changes
     except gerrit_util.GerritError, e:
       logging.error('Looking up %r: %s', instance['url'], e)
       return []
@@ -430,6 +438,7 @@ class MyActivity(object):
     url = ('https://monorail-prod.appspot.com/_ah/api/monorail/v1/projects'
            '/%s/issues/%s/comments') % (project, issue_id)
     _, body = http.request(url)
+    self.progress()
     content = json.loads(body)
     if not content:
       logging.error('Unable to parse %s response from monorail.', project)
@@ -453,6 +462,7 @@ class MyActivity(object):
     query_data = urllib.urlencode(query)
     url = url + '?' + query_data
     _, body = http.request(url)
+    self.progress()
     content = json.loads(body)
     if not content:
       logging.error('Unable to parse %s response from projecthosting.', project)
@@ -812,6 +822,10 @@ def main():
       action='store_true',
       help='Fetch deltas for changes.')
   parser.add_option(
+      '-p', '--progress',
+      action='store_true',
+      help='Display dots that show data loading progress to the user.')
+  parser.add_option(
       '--no-referenced-issues',
       action='store_true',
       help='Do not fetch issues referenced by owned changes. Useful in '
@@ -966,6 +980,9 @@ def main():
   logging.info('Using range %s to %s', options.begin, options.end)
 
   my_activity = MyActivity(options)
+  if options.progress:
+    sys.stdout.write('Loading data')
+    sys.stdout.flush()
 
   if not (options.changes or options.reviews or options.issues or
           options.changes_by_issue):
@@ -993,6 +1010,9 @@ def main():
       my_activity.get_referenced_issues()
   except auth.AuthenticationError as e:
     logging.error('auth.AuthenticationError: %s', e)
+
+  if options.progress:
+    print
 
   output_file = None
   try:

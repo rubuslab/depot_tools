63ed3460cd90bc512ca57acef7d4d3b9309c629c
John Budorick <jbudorick@chromium.org>
2018-03-18 15:37:25 -0700

    [gclient] Use a single CIPD root for an entire gclient checkout.
    gclient previously used a CIPD root per DEPS file. This didn't work
    in cases in which a DEPS file wanted to specify a CIPD package outside
    of its directory hierarchy, though, as is the case with flattened
    buildspecs.
    
    Bug: 755920
    Change-Id: I0d6c3db567f17f9171c0feaaf9ed6bc64db22757

--- tests/gclient_smoketest.py
+++ tests/gclient_smoketest.py
@@ -1256,7 +1256,7 @@ class GClientSmokeGIT(GClientSmokeBase):
         '  },',
         '',
         '  # src -> src/cipd_dep:package0',
-        '  "src/cipd_dep:package0": {',
+        '  "src/cipd_dep": {',
         '    "packages": [',
         '      {',
         '        "package": "package0",',

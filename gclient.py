3589b92f9adf56d436404e45859d2f933d499d39
Michael Moss <mmoss@google.com>
2018-04-06 18:37:19 -0700

    Apply parent vars to recursedeps.
    This fixes an issue where cumulative conditions would fail to evaluate
    if the recursed DEP (or custom_var overrides) didn't happen to redeclare
    all the variables used by the parent conditions.
    
    R=dpranke@google.com, ehmaldonado@google.com
    
    Bug: 825063
    Change-Id: Icb53f04928f914dfacc2c3035d01be103d9f8247

--- gclient.py
+++ gclient.py
@@ -1369,8 +1369,15 @@ class Dependency(gclient_utils.WorkItem, DependencySettings):
         'checkout_x64': 'x64' in self.target_cpu,
         'host_cpu': detect_host_arch.HostArch(),
     }
-    # Variables defined in DEPS file override built-in ones.
+    # Variable precedence:
+    # - built-in
+    # - DEPS vars
+    # - parents, from first to last
+    # - custom_vars overrides
     result.update(self._vars)
+    if self.parent:
+      parent_vars = self.parent.get_vars()
+      result.update(parent_vars)
     result.update(self.custom_vars or {})
     return result
 

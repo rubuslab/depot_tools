60102b2316861f91665521294b358cb757b39fe5
Edward Lemur <ehmaldonado@chromium.org>
2018-06-05 14:50:02 -0400

    gclient: Add a test to make sure we're syncing CIPD dependencies.
    Bug: 849374
    Change-Id: I5654ed65843c6dae6b7dbee10a40aa35e16803dc

---
+++ testing_support/fake_cipd.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+import shutil
+import sys
+
+
+def main():
+  assert sys.argv[1] == 'ensure'
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-ensure-file')
+  parser.add_argument('-root')
+  args, _ = parser.parse_known_args()
+
+  cipd_root = os.path.join(args.root, '.cipd')
+  if not os.path.isdir(cipd_root):
+    os.makedirs(cipd_root)
+
+  if args.ensure_file:
+    shutil.copy(args.ensure_file, os.path.join(cipd_root, 'ensure'))
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())

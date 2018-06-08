57c3e9f844b05abb00e1f193c3dfeb2af4099d1b
Jean-Marie Henaff <jmhenaff@google.com>
2018-06-08 09:36:31 +0200

    Add gsutils.bat for Windows
    Change-Id: I4a2fe49d56066b35f02e2c08c7e5e9e6f61d7df3

---
+++ gsutil.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Copyright (c) 2017, the R8 project authors. Please see the AUTHORS file
+:: for details. All rights reserved. Use of this source code is governed by a
+:: BSD-style license that can be found in the LICENSE file.
+setlocal
+
+PATH=%~dp0;%PATH%
+
+::python "c:\Users\jmhenaff\work\depot_tools\gsutil.py" %*
+python "%~dp0\gsutil.py" %*

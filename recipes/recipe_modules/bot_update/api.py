ac99d67adb1650028a29ba399945ed9a65428b6c
Robert Iannucci <iannucci@chromium.org>
2018-06-18 17:09:54 -0700

    Update bot_update and git recipe modules to use the git cache envvar instead of --cache-dir
    R=agable@chromium.org, ehmaldonado@chromium.org, tandrii@chromium.org
    
    Change-Id: I9c3bf2868dd722f2ad534652012ea328e52801da

--- recipes/recipe_modules/bot_update/api.py
+++ recipes/recipe_modules/bot_update/api.py
@@ -137,7 +137,6 @@ class BotUpdateApi(recipe_api.RecipeApi):
             self.m.gclient.config_to_pythonish(cfg))],
         ['--patch_root', root],
         ['--revision_mapping_file', self.m.json.input(reverse_rev_map)],
-        ['--git-cache-dir', cfg.cache_dir],
         ['--cleanup-dir', self.m.path['cleanup'].join('bot_update')],
 
         # How to find the patch, if any
@@ -238,12 +237,13 @@ class BotUpdateApi(recipe_api.RecipeApi):
     # Ah hah! Now that everything is in place, lets run bot_update!
     step_result = None
     try:
-      # 87 and 88 are the 'patch failure' codes for patch download and patch
-      # apply, respectively. We don't actually use the error codes, and instead
-      # rely on emitted json to determine cause of failure.
-      step_result = self(
-           name, cmd, step_test_data=step_test_data,
-           ok_ret=(0, 87, 88), **kwargs)
+      with self.m.context(env={'GIT_CACHE_PATH': cfg.cache_dir}):
+        # 87 and 88 are the 'patch failure' codes for patch download and patch
+        # apply, respectively. We don't actually use the error codes, and
+        # instead rely on emitted json to determine cause of failure.
+        step_result = self(
+             name, cmd, step_test_data=step_test_data,
+             ok_ret=(0, 87, 88), **kwargs)
     except self.m.step.StepFailure as f:
       step_result = f.result
       raise

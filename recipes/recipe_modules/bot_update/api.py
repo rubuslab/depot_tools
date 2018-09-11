0c52f6c686dc7dad5349de3dc15fb675cadb4931
Nodir Turakulov <nodir@google.com>
2018-09-10 17:04:20 -0700

    [bot_update] Use get_repo_path in _destination_branch
    Switch to gclient.get_repo_path in bot_update.api._destination_branch.
    This removes on patch_project usage.
    
    Recipe-Nontrivial-Roll: infra
    Bug: 694348
    Change-Id: If05371d0f6d11be6803ffa8392e029f5860dda25

--- recipes/recipe_modules/bot_update/api.py
+++ recipes/recipe_modules/bot_update/api.py
@@ -336,13 +336,13 @@ class BotUpdateApi(recipe_api.RecipeApi):
         and if different from master, returns 'HEAD' otherwise.
     """
     # Bail out if this is not a gerrit change.
-    if not self.m.tryserver.gerrit_change:
+    if not self.m.tryserver.gerrit_change_repo_url:
       return 'HEAD'
 
     # Ignore other project paths than the one belonging to the CL.
-    if path != cfg.patch_projects.get(
-        self.m.properties.get('patch_project'),
-        (cfg.solutions[0].name, None))[0]:
+    patch_path = self.m.gclient.get_repo_path(
+        self.m.tryserver.gerrit_change_repo_url, gclient_config=cfg)
+    if path != patch_path:
       return 'HEAD'
 
     target_ref = self.m.tryserver.gerrit_change_target_ref

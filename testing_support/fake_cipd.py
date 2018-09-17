699dbd0c193c52bc7c4e3168135dc49b7dd9ef81
Edward Lemur <ehmaldonado@chromium.org>
2018-09-17 17:04:53 -0400

    gclient: delete unversioned directory before adding cipd dep for the same path
    Bug: 882611
    Change-Id: I46e41cc9693b90874b5d6569a12ec638eaac1050

--- testing_support/fake_cipd.py
+++ testing_support/fake_cipd.py
@@ -5,8 +5,25 @@
 
 import argparse
 import os
+import re
 import shutil
 import sys
+
+
+CIPD_SUBDIR_RE = '@Subdir (.*)'
+
+
+def parse_cipd(root, contents):
+  tree = {}
+  current_subdir = None
+  for line in contents:
+    line = line.strip()
+    match = re.match(CIPD_SUBDIR_RE, line)
+    if match:
+      current_subdir = os.path.join(root, *match.group(1).split('/'))
+    elif line and current_subdir:
+      tree.setdefault(current_subdir, []).append(line)
+  return tree
 
 
 def main():
@@ -16,12 +33,31 @@ def main():
   parser.add_argument('-root')
   args, _ = parser.parse_known_args()
 
-  cipd_root = os.path.join(args.root, '.cipd')
-  if not os.path.isdir(cipd_root):
-    os.makedirs(cipd_root)
+  cipd_path = os.path.join(args.root, '_cipd')
+  if os.path.exists(cipd_path):
+    with open(cipd_path) as f:
+      old_content = parse_cipd(args.root, f.readlines())
+  else:
+    old_content = {}
 
-  if args.ensure_file:
-    shutil.copy(args.ensure_file, os.path.join(cipd_root, 'ensure'))
+  with open(args.ensure_file) as f:
+    new_content = parse_cipd(args.root, f.readlines())
+
+  # Remove old packages
+  for path in old_content:
+    os.remove(os.path.join(path, '_cipd'))
+    if not os.listdir(path):
+      os.rmdir(path)
+
+  # Install new packages
+  for path, packages in new_content.iteritems():
+    if not os.path.exists(path):
+      os.makedirs(path)
+    with open(os.path.join(path, '_cipd'), 'w') as f:
+      f.write('\n'.join(packages))
+
+  # Save the ensure file that we got
+  shutil.copy(args.ensure_file, cipd_path)
 
   return 0
 

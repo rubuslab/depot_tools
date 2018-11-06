c434212f57b2fa9214c4660c4fdab4b73741267d
Aiden Benner <abenner@google.com>
2018-11-06 14:15:26 -0500

    Enable python formatting by defaults if yapf file provided
    Enables python formatting git cl format without the
    --python flag for all files that have a .style.yapf file
    found in a parent directory up to the repository root.
    
    This allows default formatting to be enabled on a per
    project/directory basis.
    
    Bug: 898261
    Change-Id: I40d899ec1a3e0dfca445e04b91befab113416175

--- presubmit_canned_checks.py
+++ presubmit_canned_checks.py
@@ -1060,18 +1060,22 @@ def PanProjectChecks(input_api, output_api,
   return results
 
 
-def CheckPatchFormatted(
-    input_api, output_api, check_js=False, check_python=False,
-    result_factory=None):
+def CheckPatchFormatted(input_api,
+                        output_api,
+                        check_js=False,
+                        check_python=None,
+                        result_factory=None):
   result_factory = result_factory or output_api.PresubmitPromptWarning
   import git_cl
 
   display_args = []
   if check_js:
     display_args.append('--js')
-  if check_python:
-    # --python requires --full
-    display_args.extend(['--python', '--full'])
+  if check_python is not None:
+    if check_python:
+      display_args.extend(['--python'])
+    else:
+      display_args.extend(['--nopython'])
 
   cmd = ['-C', input_api.change.RepositoryRoot(),
          'cl', 'format', '--dry-run', '--presubmit'] + display_args

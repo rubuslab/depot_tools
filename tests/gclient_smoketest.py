13396d4b13911ab4fca36f7f02f7ff0338359620
Eric Boren <borenet@google.com>
2018-11-16 12:17:26 -0500

    [gclient] Make getdep and setdep to provide builtin vars
    Bug: 906114
    Change-Id: I069cc21343911f7fdb3c91ecbd8fcba53fc8099f

--- tests/gclient_smoketest.py
+++ tests/gclient_smoketest.py
@@ -881,6 +881,7 @@ class GClientSmokeGIT(GClientSmokeBase):
     self.assertEqual(out, output_json)
 
   def testSetDep(self):
+    self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'])
     fake_deps = os.path.join(self.root_dir, 'DEPS.fake')
     with open(fake_deps, 'w') as f:
       f.write('\n'.join([
@@ -894,6 +895,11 @@ class GClientSmokeGIT(GClientSmokeBase):
           '  },',
           '  "bar": "url@bar_rev",',
           '}',
+          'hooks = [{',
+          '  "name": "uses_builtin_var",',
+          '  "pattern": ".",',
+          '  "action": ["python", "fake.py", "--with-android={checkout_android}"],',
+          '}]',
       ]))
 
     results = self.gclient([
@@ -916,9 +922,15 @@ class GClientSmokeGIT(GClientSmokeBase):
           '  },',
           '  "bar": "url@new_bar",',
           '}',
+          'hooks = [{',
+          '  "name": "uses_builtin_var",',
+          '  "pattern": ".",',
+          '  "action": ["python", "fake.py", "--with-android={checkout_android}"],',
+          '}]',
     ], contents)
 
   def testGetDep(self):
+    self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'])
     fake_deps = os.path.join(self.root_dir, 'DEPS.fake')
     with open(fake_deps, 'w') as f:
       f.write('\n'.join([
@@ -932,6 +944,11 @@ class GClientSmokeGIT(GClientSmokeBase):
           '  },',
           '  "bar": "url@bar_rev",',
           '}',
+          'hooks = [{',
+          '  "name": "uses_builtin_var",',
+          '  "pattern": ".",',
+          '  "action": ["python", "fake.py", "--with-android={checkout_android}"],',
+          '}]',
       ]))
 
     results = self.gclient([

21d1414c23ca7e62e0c9a0a16ef02a8606d2c012
Johannes Henkel <johannes@chromium.org>
2018-11-16 11:09:17 -0800

    Change-Id: Ifc97524c092262c63fdadf74a0dbbe96f643c96e
    [DevTools] Add a fetch config for the inspector_protocol.
    
    This will facilitate a more standalone development process.
    We don't have a DEPS file there yet, but I'm planning
    to add a simple one, which for starters will
    have gunit. I'll model this mostly after breakpad.
    I've tested this fetch config and it appears to work great:
    
    $ mkdir testfetch
    $ cd testfetch
    $ fetch inspector_protocol
    
    After this there's an src directory in testfetch which
    has our sources.
    
    Change-Id: I482d65b3d8e50e9e5a98c4affc7b487a5bd6df53

--- fetch_configs/inspector_protocol.py
+++ fetch_configs/inspector_protocol.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 The Chromium Authors. All rights reserved.
+# Copyright (c) 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -9,10 +9,10 @@ import config_util  # pylint: disable=import-error
 
 # This class doesn't need an __init__ method, so we disable the warning
 # pylint: disable=no-init
-class Breakpad(config_util.Config):
+class InspectorProtocol(config_util.Config):
   @staticmethod
   def fetch_spec(props):
-    url = 'https://chromium.googlesource.com/breakpad/breakpad.git'
+    url = 'https://chromium.googlesource.com/deps/inspector_protocol/'
     solution = {
       'name': 'src',
       'url': url,
@@ -22,10 +22,6 @@ class Breakpad(config_util.Config):
     spec = {
       'solutions': [solution],
     }
-    if props.get('target_os'):
-      spec['target_os'] = props['target_os'].split(',')
-    if props.get('target_os_only'):
-      spec['target_os_only'] = props['target_os_only']
     return {
       'type': 'gclient_git',
       'gclient_git_spec': spec,
@@ -37,7 +33,7 @@ class Breakpad(config_util.Config):
 
 
 def main(argv=None):
-  return Breakpad().handle_args(argv)
+  return InspectorProtocol().handle_args(argv)
 
 
 if __name__ == '__main__':

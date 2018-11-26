bb3c22b58abac5b71ca1a5bd85eb0bba07fc4571
Joe Mason <joenotcharles@google.com>
2018-11-26 17:22:08 -0500

    More context when _load_codereview_impl asserts
    R=tandrii
    
    Bug: 908466
    Change-Id: I715633c64fff343d07dbcf50c261e686d77e8c44

--- git_cl.py
+++ git_cl.py
@@ -1159,7 +1159,8 @@ class Changelist(object):
 
   def _load_codereview_impl(self, codereview=None, **kwargs):
     if codereview:
-      assert codereview in _CODEREVIEW_IMPLEMENTATIONS
+      assert codereview in _CODEREVIEW_IMPLEMENTATIONS, "codereview {} not in [{}]".format(
+          codereview, _CODEREVIEW_IMPLEMENTATIONS)
       cls = _CODEREVIEW_IMPLEMENTATIONS[codereview]
       self._codereview = codereview
       self._codereview_impl = cls(self, **kwargs)

525c4fa157100fb317cb43c25e8134a31270dc82
Clemens Hammacher <clemensh@chromium.org>
2018-12-04 11:35:50 +0100

    Update documentation of `git new-branch`
    Mention that rebasing is sometimes needed after creating a branch. Also
    show this in the example. Include verbose output of `git map-branches`
    to make visible what the state of the different branches is.
    
    R=iannucci@chromium.org
    
    Change-Id: I4af03ef15b39e4ba84a41c01d1ec801e7bb4bbd6

--- man/src/git-new-branch.demo.1.sh
+++ man/src/git-new-branch.demo.1.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 . demo_repo.sh
 
-run git map-branches
+run git map-branches -v
+run git checkout origin/master
+praw vi foo \&\& git add -A \&\& git commit -m 'foo'
+silent echo 'foo' >foo
+silent git add -A
+silent git commit -m 'foo'
 run git new-branch independent_cl
-run git map-branches
+run git map-branches -v
 run git new-branch --upstream subfeature nested_cl
+run git map-branches -v
+run git rebase @{upstream}
+run git map-branches -v
 callout 3
-run git map-branches
 run git checkout cool_feature 2>&1
 run git new-branch --upstream_current cl_depends_on_cool_feature
-run git map-branches
+run git map-branches -v

aa33123ab335ebb0550e97a50665d3299b7b02f1
Tim Schumacher <timschumi@gmx.de>
2019-03-07 07:39:01 +0100

    Specify python2 everywhere

--- third_party/boto/pyami/launch_ami.py
+++ third_party/boto/pyami/launch_ami.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # Copyright (c) 2006,2007 Mitch Garnaat http://garnaat.org/
 #
 # Permission is hereby granted, free of charge, to any person obtaining a

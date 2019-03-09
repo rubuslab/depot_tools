18f78563e1a6f3be65e6690f4e73009906e5af78
Nico Weber <thakis@chromium.org>
2019-03-09 18:21:42 -0500

    //static_assert(1, ""); try something
    .
    
    Change-Id: I9c9a775b58a79a985f6fc85d0a1c98aa337601c4

--- gclient.py
+++ gclient.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+
 """Meta checkout dependency manager for Git."""
 # Files
 #   .gclient      : Current client configuration, written by 'config' command.
--- gclient_utils.py
+++ gclient_utils.py
@@ -4,6 +4,7 @@
 
 """Generic utils."""
 
+
 import codecs
 import collections
 import contextlib

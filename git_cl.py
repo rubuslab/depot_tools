198e1d39d7470df08af0a4892f9ed3a209845460
sangwoo.ko <sangwoo.ko@navercorp.com>
2019-05-21 13:23:53 +0900

    WIP
    Change-Id: I2733849735adc0361140b042ce617522c58fcc78

--- git_cl.py
+++ git_cl.py
@@ -2551,7 +2551,7 @@ class _GerritChangelistImpl(_ChangelistCodereviewBase):
     git_info_dir = tempfile.mkdtemp()
     git_info_zip = trace_name + '-git-info'
 
-    git_push_metadata['now'] = datetime_now().strftime('%c')
+    git_push_metadata['now'] = datetime_now().strftime('%x')
     git_push_metadata['trace_name'] = trace_name
     gclient_utils.FileWrite(
         traces_readme, TRACES_README_FORMAT % git_push_metadata)

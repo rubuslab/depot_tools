b7b801583c443c9074ce7d79af5c0b731fa36aae
Debrian Figueroa <debrian@google.com>
2019-06-18 16:10:51 -0700

    Augment presubmit_support to output a json summary.
    Adds functionality to presubmit api to return a summary of the
    errors caught in presubmit support in the form of a json.
    
    Bug:971895
    Change-Id: I42a3df3994077342216d002381b6135012b4334c

--- recipes/recipe_modules/presubmit/api.py
+++ recipes/recipe_modules/presubmit/api.py
@@ -13,7 +13,7 @@ class PresubmitApi(recipe_api.RecipeApi):
     """Return a presubmit step."""
 
     name = kwargs.pop('name', 'presubmit')
-
     with self.m.depot_tools.on_path():
-      return self.m.python(
-          name, self.presubmit_support_path, list(args), **kwargs)
+      step_data = self.m.python(
+          name, self.presubmit_support_path, list(args) + ['--dump_json', self.m.json.output()], **kwargs)
+      return step_data.json.output

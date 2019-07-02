992100fe60c443fe55542d893e683dee3d93e18a
John Budorick <jbudorick@chromium.org>
2019-07-02 11:39:16 -0700

    Copy presubmit logic up into the presubmit recipe module.
    Bug: 925774
    Change-Id: I27e8670578095db71384d77cdb3d606bfdcf1077

---
+++ recipes/recipe_modules/presubmit/test_api.py
@@ -0,0 +1,21 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine import recipe_test_api
+
+class PresubmitTestApi(recipe_test_api.RecipeTestApi):
+
+  def __call__(self, clear_pythonpath=False, runhooks=False, timeout_s=480,
+               vpython_spec_path=None):
+    ret = self.test(None)
+    ret.properties = {
+        '$depot_tools/presubmit': {
+            'clear_pythonpath': clear_pythonpath,
+            'runhooks': runhooks,
+            'timeout_s': timeout_s,
+            'vpython_spec_path': vpython_spec_path,
+        },
+    }
+
+    return ret

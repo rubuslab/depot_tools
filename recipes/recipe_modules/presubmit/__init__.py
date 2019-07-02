6c16df5d2347a82aa83c01ac9172b515fc0f20d3
John Budorick <jbudorick@chromium.org>
2019-07-02 11:08:46 -0700

    Copy presubmit logic up into the presubmit recipe module.
    Bug: 925774
    Change-Id: I27e8670578095db71384d77cdb3d606bfdcf1077

--- recipes/recipe_modules/presubmit/__init__.py
+++ recipes/recipe_modules/presubmit/__init__.py
@@ -1,8 +1,36 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine.config import ConfigGroup, Single
+from recipe_engine.recipe_api import Property
+
+
 DEPS = [
+  'bot_update',
   'depot_tools',
-  'recipe_engine/json',
+  'gclient',
+  'git',
   'recipe_engine/context',
+  'recipe_engine/cq',
+  'recipe_engine/json',
   'recipe_engine/path',
+  'recipe_engine/properties',
   'recipe_engine/python',
   'recipe_engine/step',
+  'tryserver',
 ]
+
+
+PROPERTIES = {
+    '$depot_tools/presubmit': Property(
+        help='Properties for the presubmit module',
+        param_name='properties',
+        kind=ConfigGroup(
+            clear_pythonpath=Single(bool),
+            timeout_s=Single(int),
+            vpython_spec_path=Single(str),
+        ),
+        default={},
+    ),
+}

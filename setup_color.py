7439abc6d11372eae596b6e65d3986e97af8910d
Raul Tambre <raul@tambre.ee>
2019-08-20 09:31:06 +0300

    setup_color: Use native ANSI support on Windows 10
    Windows 10 has support for native ANSI control codes.
    This means we don't have to wrap stdout/stderr with Colorama, which uses old CMD APIs to handle these sequences.
    
    This change inadvertently also works around an issue on Python 3 where Colorama errors when bytes are output by gclient_utils.CheckCallAndFilter due to not wrapping sys.stdout.buffer.
    
    Bug: 942522
    Change-Id: Ic7dde620b01bd94eb50de353bfc0276cad045a45

--- setup_color.py
+++ setup_color.py
@@ -5,7 +5,9 @@
 
 from __future__ import print_function
 
+import ctypes
 import os
+import platform
 import sys
 from third_party import colorama
 
@@ -30,7 +32,6 @@ def init():
     OUT_TYPE = 'file (win)'
 
     import msvcrt
-    import ctypes
     h = msvcrt.get_osfhandle(sys.stdout.fileno())
     # h is the win32 HANDLE for stdout.
     ftype = ctypes.windll.kernel32.GetFileType(h)
@@ -84,6 +85,23 @@ def init():
     else:
       # A normal file, or an unknown file type.
       pass
+
+    # Enable native ANSI color codes on Windows 10.
+    if IS_TTY and platform.release() == '10':
+      # Don't need Colorama wrapping.
+      should_wrap = False
+
+      import subprocess
+      kernel32 = ctypes.windll.kernel32
+      ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x04
+
+      out_handle = kernel32.GetStdHandle(subprocess.STD_OUTPUT_HANDLE)
+      mode = ctypes.wintypes.DWORD()
+      kernel32.GetConsoleMode(out_handle, ctypes.byref(mode))
+
+      if not (mode.value & ENABLE_VIRTUAL_TERMINAL_PROCESSING):
+        kernel32.SetConsoleMode(out_handle,
+                                mode.value | ENABLE_VIRTUAL_TERMINAL_PROCESSING)
   else:
     # This is non-windows, so we trust isatty.
     OUT_TYPE = 'pipe or file'

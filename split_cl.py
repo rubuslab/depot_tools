8d2e16427727e1fbf8b8e52984d2bfd54f43dd87
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-08-31 18:03:07 +0200

    Improve git cl split
    Change-Id: I49f868972a61b89b426ef9e2ceedc733eacb4350

--- split_cl.py
+++ split_cl.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 import collections
 import os
+import random
 import re
 import subprocess2
 import sys
@@ -19,6 +20,8 @@ import owners
 import owners_finder
 
 import git_common as git
+
+import third_party.pygtrie as trie
 
 
 # If a call to `git cl split` will generate more than this number of CLs, the
@@ -75,12 +78,13 @@ def AddUploadedByGitClSplitToDescription(description):
   return '\n'.join(lines)
 
 
-def UploadCl(refactor_branch, refactor_branch_upstream, directory, files,
-             description, comment, reviewers, changelist, cmd_upload,
+def UploadCl(cl_index, refactor_branch, refactor_branch_upstream, directory,
+             files, description, comment, reviewer, changelist, cmd_upload,
              cq_dry_run, enable_auto_submit):
   """Uploads a CL with all changes to |files| in |refactor_branch|.
 
   Args:
+    cl_index: The index of this CL in the list of CLs to upload.
     refactor_branch: Name of the branch that contains the changes to upload.
     refactor_branch_upstream: Name of the upstream of |refactor_branch|.
     directory: Path to the directory that contains the OWNERS file for which
@@ -88,15 +92,15 @@ def UploadCl(refactor_branch, refactor_branch_upstream, directory, files,
     files: List of AffectedFile instances to include in the uploaded CL.
     description: Description of the uploaded CL.
     comment: Comment to post on the uploaded CL.
-    reviewers: A set of reviewers for the CL.
+    reviewer: The reviewer for the CL.
     changelist: The Changelist class.
     cmd_upload: The function associated with the git cl upload command.
     cq_dry_run: If CL uploads should also do a cq dry run.
     enable_auto_submit: If CL uploads should also enable auto submit.
   """
   # Create a branch.
-  if not CreateBranchForDirectory(
-      refactor_branch, directory, refactor_branch_upstream):
+  if not CreateBranchForDirectory(refactor_branch, str(cl_index),
+                                  refactor_branch_upstream):
     print('Skipping ' + directory + ' for which a branch already exists.')
     return
 
@@ -119,7 +123,7 @@ def UploadCl(refactor_branch, refactor_branch_upstream, directory, files,
     os.remove(tmp_file.name)
 
   # Upload a CL.
-  upload_args = ['-f', '-r', ','.join(reviewers)]
+  upload_args = ['-f', '-r', reviewer]
   if cq_dry_run:
     upload_args.append('--cq-dry-run')
   if not comment:
@@ -133,22 +137,135 @@ def UploadCl(refactor_branch, refactor_branch_upstream, directory, files,
                             publish=True)
 
 
-def GetFilesSplitByOwners(owners_database, files):
+class ChangeList(object):
+  """Representation of a CL and the files affected by it."""
+
+  def __init__(self, path, owners_db, author, files):
+    self._path = path
+    self._files = files
+    self._owners_db = owners_db
+    self._author = author
+    self._owners = None
+
+  def _EnsureOwners(self):
+    if not self._owners:
+      self._owners = set()
+      files = [f.LocalPath() for f in self.GetFiles()]
+      if not files:
+        files = [self.GetPath()]
+      possible_owners = self._owners_db.all_possible_owners(
+          files, self._author).keys()
+      for owner in possible_owners:
+        if 0 == len(self._owners_db.files_not_covered_by(files, [owner])):
+          self._owners |= set([owner])
+      assert len(self._owners)
+
+  def Merge(self, other):
+    self._owners = self.GetCommonOwners(other)
+    self._files |= other.GetFiles()
+
+  def GetPath(self):
+    return self._path
+
+  def GetFiles(self):
+    return self._files
+
+  def GetOwners(self):
+    self._EnsureOwners()
+    return self._owners
+
+  def GetCommonOwners(self, other):
+    return self.GetOwners() & other.GetOwners()
+
+  def HaveCommonOwners(self, other):
+    return len(self.GetCommonOwners(other)) > 0
+
+  def GetChangeSizeInBytes(self):
+    return sum(
+        [c[0] + c[1] for f in self._files for c in f.ChangeSizeInBytes()])
+
+
+def SplitCLs(owners_database, author, files):
   """Returns a map of files split by OWNERS file.
 
   Returns:
     A map where keys are paths to directories containing an OWNERS file and
     values are lists of files sharing an OWNERS file.
   """
-  files_split_by_owners = collections.defaultdict(list)
+
+  # The target CL size in # of changed bytes.
+  # TODO(yannic): Use # of changed lines instead and make this configurable.
+  max_cl_size = 1000
+
+  candidates = trie.Trie()
+  # Enable sorting so dry-run will split the CL the same way the CL is uploaded.
+  candidates.enable_sorting()
+
+  # 1. Create one CL candidate for every affected file.
   for f in files:
-    files_split_by_owners[owners_database.enclosing_dir_with_owners(
-        f.LocalPath())].append(f)
-  return files_split_by_owners
+    path = f.LocalPath()
+    candidates[path] = ChangeList(path, owners_database, author, set([f]))
+
+  change_lists = []
+
+  # 2. Try to merge CL in common directories up to a maximum size of
+  # |max_cl_size|.
+  # This is O( len(files) * max([len(f.path) for f in files]) ).
+  edited = True
+  while edited:
+    edited = False
+
+    # 2.1. Iterate over all candidates and merge candidates into the candidate
+    # for their parent directory if the resulting CL doesn't exceed
+    # |max_cl_size|.
+    for item in candidates.items():
+      path = ''.join(item[0])
+      candidate = item[1]
+
+      # The number of CL candidates in subdirectories is equivalent to the
+      # number of nodes with prefix |path| in the Trie.
+      # Only try to merge |candidate| with the candidate for the parent
+      # directory if there are no more CLs for subdirectories.
+      sub_cls = len([''.join(k) for k in candidates.keys(path)]) - 1
+      if not sub_cls:
+        parent_path = os.path.dirname(path)
+        if len(parent_path) < 1:
+          # Don't create CLs for more than one top-level directory.
+          continue
+
+        if parent_path not in candidates:
+          candidates[parent_path] = ChangeList(parent_path, owners_database,
+                                               author, set())
+        parent_cl = candidates[parent_path]
+
+        if not parent_cl.HaveCommonOwners(candidate):
+          # Don't merge if the resulting CL would need more than one reviewer.
+          continue
+
+        # Merge |candidate| into the CL for it's parent directory and remove
+        # candidate.
+        edited = True
+        del candidates[path]
+        parent_cl.Merge(candidate)
+
+        # Add |parent_cl| to list of CLs to submit if the CL is larger than
+        # |max_cl_size|.
+        # TODO(yannic): Doing it this way, we might end up with CLs of size
+        # 2 * max_cl_size if we merged two candidates that just don't exceed
+        # the maximal size.
+        if parent_cl.GetChangeSizeInBytes() > max_cl_size:
+          change_lists.append(parent_cl)
+          del candidates[parent_path]
+
+  # 3. Add all remaining candidates to the list of CLs.
+  for item in candidates.items():
+    change_lists.append(item[1])
+
+  return change_lists
 
 
 def PrintClInfo(cl_index, num_cls, directory, file_paths, description,
-                reviewers):
+                reviewer):
   """Prints info about a CL.
 
   Args:
@@ -158,7 +275,7 @@ def PrintClInfo(cl_index, num_cls, directory, file_paths, description,
         to upload a CL.
     file_paths: A list of files in this CL.
     description: The CL description.
-    reviewers: A set of reviewers for this CL.
+    reviewer: The reviewer for this CL.
   """
   description_lines = FormatDescriptionOrComment(description,
                                                  directory).splitlines()
@@ -166,10 +283,26 @@ def PrintClInfo(cl_index, num_cls, directory, file_paths, description,
 
   print('CL {}/{}'.format(cl_index, num_cls))
   print('Path: {}'.format(directory))
-  print('Reviewers: {}'.format(', '.join(reviewers)))
+  print('Reviewers: {}'.format(reviewer))
   print('\n' + indented_description + '\n')
   print('\n'.join(file_paths))
   print()
+
+
+def _SelectReviewer(owners, used_reviwers=set()):
+  """Select a reviewer from |owners| and adds them to the set of used reviewers.
+
+  Returns:
+    The reviewer.
+  """
+  unused_reviewers = owners.difference(used_reviwers)
+  if len(unused_reviewers) < 1:
+    unused_reviewers = owners
+  # Pick a random reviwer from the set of owners so we don't prefer owners
+  # with emails of low lexical order.
+  reviewer = random.choice(tuple(unused_reviewers))
+  used_reviwers.add(reviewer)
+  return reviewer
 
 
 def SplitCl(description_file, comment_file, changelist, cmd_upload, dry_run,
@@ -212,11 +345,9 @@ def SplitCl(description_file, comment_file, changelist, cmd_upload, dry_run,
     owners_database = owners.Database(change.RepositoryRoot(), file, os.path)
     owners_database.load_data_needed_for([f.LocalPath() for f in files])
 
-    files_split_by_owners = GetFilesSplitByOwners(owners_database, files)
+    change_lists = SplitCLs(owners_database, author, set(files))
 
-    num_cls = len(files_split_by_owners)
-    print('Will split current branch (' + refactor_branch + ') into ' +
-          str(num_cls) + ' CLs.\n')
+    num_cls = len(change_lists)
     if cq_dry_run and num_cls > CL_SPLIT_FORCE_LIMIT:
       print(
         'This will generate "%r" CLs. This many CLs can potentially generate'
@@ -228,20 +359,20 @@ def SplitCl(description_file, comment_file, changelist, cmd_upload, dry_run,
       if answer.lower() != 'y':
         return 0
 
-    for cl_index, (directory, files) in \
-        enumerate(files_split_by_owners.iteritems(), 1):
+    reviewers = set()
+    for cl_index, cl in enumerate(change_lists, 1):
       # Use '/' as a path separator in the branch name and the CL description
       # and comment.
-      directory = directory.replace(os.path.sep, '/')
-      file_paths = [f.LocalPath() for f in files]
-      reviewers = owners_database.reviewers_for(file_paths, author)
+      directory = cl.GetPath().replace(os.path.sep, '/')
+      file_paths = [f.LocalPath() for f in cl.GetFiles()]
+      reviewer = _SelectReviewer(cl.GetOwners(), reviewers)
 
       if dry_run:
         PrintClInfo(cl_index, num_cls, directory, file_paths, description,
-                    reviewers)
+                    reviewer)
       else:
-        UploadCl(refactor_branch, refactor_branch_upstream, directory, files,
-                 description, comment, reviewers, changelist, cmd_upload,
+        UploadCl(cl_index, refactor_branch, refactor_branch_upstream, directory,
+                 files, description, comment, reviewer, changelist, cmd_upload,
                  cq_dry_run, enable_auto_submit)
 
     # Go back to the original branch.

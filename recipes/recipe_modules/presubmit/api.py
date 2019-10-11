3728d92aa56062b51c2b9e7d088dc2149e8cd1ad
Junji Watanabe <jwata@google.com>
2019-10-11 18:04:59 +0900

    [depot_tools] use vpython for presubmit by default
    presubmit_support.py should run using vpython by default.
    So caller don't need to always specify venv=True.
    
    Related CL: crrev.com/c/1855163
    
    Change-Id: I2548b4efa5f2e2d65ab076f183989147784b9493
    Recipe-Nontrivial-Roll: build

--- recipes/recipe_modules/presubmit/api.py
+++ recipes/recipe_modules/presubmit/api.py
@@ -32,6 +32,8 @@ class PresubmitApi(recipe_api.RecipeApi):
       presubmit_args = list(args) + [
           '--json_output', self.m.json.output(),
       ]
+      # always use vpython and depot_tools/.vpython
+      kwargs['venv'] = True
       step_data = self.m.python(
           name, self.presubmit_support_path, presubmit_args, **kwargs)
       return step_data.json.output

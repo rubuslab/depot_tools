6d3822bd3fd1c85f75d60ac26332936b5aa36f29
Erik Chen <erikchen@chromium.org>
2019-10-29 13:02:05 -0700

    Add more git tracing.
    Change-Id: I9b69c60b6c81fc0c5f23f82fcc889b4d45a27556

--- recipes/recipe_modules/bot_update/resources/bot_update.py
+++ recipes/recipe_modules/bot_update/resources/bot_update.py
@@ -666,6 +666,10 @@ def _git_checkout(sln, sln_dir, revisions, refs, git_cache_dir, cleanup_dir):
     env = {
         'GIT_TRACE': 'true',
         'GIT_TRACE_PERFORMANCE': 'true',
+        'GIT_TRACE_PACKET': '1',
+        'GIT_TRACE2_EVENT': '1',
+        'GIT_TRACE_CURL': '1',
+        'GIT_TRACE_CURL_NO_DATA': '1'
     }
 
   # Step 1: populate/refresh cache, if necessary.

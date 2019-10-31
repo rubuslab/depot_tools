1fd19f6a2a5c955f8e60e78cf315cf28a449d5de
Edward Lemur <ehmaldonado@chromium.org>
2019-10-31 18:38:31 -0400

    gsutil: Decode output from subprocess.
    Bug: 1007872, 1009819
    Change-Id: Icb24badc0429012dc66912d7d35e03a456512787

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -70,7 +70,6 @@ def CommonChecks(input_api, output_api, tests_to_black_list, run_on_python3):
     print('Warning: skipping most unit tests on Windows')
     tests_to_black_list = [
         r'.*auth_test\.py$',
-        r'.*download_from_google_storage_unittest\.py$',
         r'.*gclient_smoketest\.py$',
         r'.*git_cl_test\.py$',
         r'.*git_common_test\.py$',

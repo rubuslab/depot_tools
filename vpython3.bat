47d1bf400b3a988bc512d7ff90fc4f8e91a85260
Edward Lemur <ehmaldonado@chromium.org>
2019-11-11 18:27:15 -0500

    stuff
    All.
    
    Change-Id: I5187943b6cf7499d50b3f1a2ec906a5e8fd84835

--- vpython3.bat
+++ vpython3.bat
@@ -3,5 +3,11 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+:: Will download git and python.
+call "%DEPOT_TOOLS_DIR%bootstrap\win_tools.bat"
+
+for /f %%i in (%~dp0python3_bin_reldir.txt) do set PYTHON3_BIN_RELDIR=%%i
+set PATH=%~dp0%PYTHON3_BIN_RELDIR%;%~dp0%PYTHON3_BIN_RELDIR%\Scripts;%PATH%
+
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
 "%~dp0\.cipd_bin\vpython3.exe" %*

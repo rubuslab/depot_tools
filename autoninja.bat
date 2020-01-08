81ab9fd6fbb48abb57b8113d3e331df2741cf3e8
Edward Lemur <ehmaldonado@google.com>
2020-01-08 15:51:41 -0500

    Run autoninja scripts using vpython.
    Bug: 1002153
    Change-Id: I769a5370d726f4ce478035f59bfd1df50fef452e

--- autoninja.bat
+++ autoninja.bat
@@ -19,14 +19,13 @@ REM Also print it to reassure that the right settings are being used.
 FOR /f "usebackq tokens=*" %%a in (`vpython %~dp0autoninja.py "%*"`) do echo %%a & %%a
 @if errorlevel 1 goto buildfailure
 
-REM Use call to invoke python script here, because we use python via python.bat.
-@if "%NINJA_SUMMARIZE_BUILD%" == "1" call python.bat %~dp0post_build_ninja_summary.py %*
-@call python.bat %~dp0ninjalog_uploader_wrapper.py --cmdline %*
+@if "%NINJA_SUMMARIZE_BUILD%" == "1" vpython %~dp0post_build_ninja_summary.py %*
+vpython %~dp0ninjalog_uploader_wrapper.py --cmdline %*
 
 exit /b
 :buildfailure
 
-@call python.bat %~dp0ninjalog_uploader_wrapper.py --cmdline %*
+vpython %~dp0ninjalog_uploader_wrapper.py --cmdline %*
 
 REM Return an error code of 1 so that if a developer types:
 REM "autoninja chrome && chrome" then chrome won't run if the build fails.

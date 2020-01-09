81bdb8f0533a0f0a5ff457dd3713733b7337af92
Edward Lemur <ehmaldonado@google.com>
2020-01-09 16:25:12 -0500

    git-map: Refactor and add simple tests
    Change-Id: I8fd0034f6a6d7623792620f92208b25961fa174e

--- vpython.bat
+++ vpython.bat
@@ -3,5 +3,10 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+echo "USING VPYTHON"
+
+for /f %%i in (%~dp0python_bin_reldir.txt) do set PYTHON_BIN_RELDIR=%%i
+set PATH=%~dp0%PYTHON_BIN_RELDIR%;%~dp0%PYTHON_BIN_RELDIR%\Scripts;%PATH%
+
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
-"%~dp0\.cipd_bin\vpython.exe" -vpython-interpreter "%~dp0\python.bat" %*
+"%~dp0\.cipd_bin\vpython.exe" -vpython-interpreter "%PYTHON_BIN_RELDIR%\python.exe" %*

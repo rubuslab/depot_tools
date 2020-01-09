06b93488b6970f834c96a6f4da1ff8b644257526
Edward Lesmes <ehmaldonado@google.com>
2020-01-09 00:20:13 +0000

    vpython: Specify interpreter to run.
    Specify the interpreter to run to avoid issues with several
    python installations on Windows.
    
    Bug: 1036606
    
    Change-Id: Ib70d041acc52c940711460219acd8f4ad58637a9

--- vpython3.bat
+++ vpython3.bat
@@ -4,4 +4,4 @@
 :: found in the LICENSE file.
 
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
-"%~dp0\.cipd_bin\vpython3.exe" %*
+"%~dp0\.cipd_bin\vpython3.exe" -vpython-interpreter "%~dp0\python3.bat" %*

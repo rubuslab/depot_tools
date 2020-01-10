13623b3ca72058a3135fb80080f6e738b0e85ae1
Olivier Li <olivierli@google.com>
2020-01-10 11:52:54 -0500

    Run depot tools tests
    Change-Id: I533cbb13069ce5c529a3039fab0385e011c23267

--- split_cl.py
+++ split_cl.py
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This is not a material change!
+
 """Splits a branch into smaller branches and uploads CLs."""
 
 from __future__ import print_function

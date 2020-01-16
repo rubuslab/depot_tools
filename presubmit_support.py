c5a5cfaf764b93309901425d25661d506dbc9b97
Edward Lemur <ehmaldonado@google.com>
2020-01-16 12:45:10 -0500

    presubmit_support: Initialize Gerrit object when gerrit URL is given.
    Bug: 1042324
    Change-Id: I79d78fb67465f989755bf8c8ceb48d665464fa05

--- presubmit_support.py
+++ presubmit_support.py
@@ -1766,6 +1766,21 @@ def main(argv=None):
 
   options, args = parser.parse_args(argv)
 
+  gerrit_obj = None
+  if options.gerrit_url:
+    gerrit_obj = GerritAccessor(urlparse.urlparse(options.gerrit_url).netloc)
+  if options.gerrit_fetch:
+    if not options.gerrit_url or not options.issue or not options.patchset:
+      parser.error(
+          '--gerrit_fetch requires --gerrit_url, --issue and --patchset.')
+
+    options.author = gerrit_obj.GetChangeOwner(options.issue)
+    options.description = gerrit_obj.GetChangeDescription(
+        options.issue, options.patchset)
+
+    logging.info('Got author: "%s"', options.author)
+    logging.info('Got description: """\n%s\n"""', options.description)
+
   if options.verbose >= 2:
     logging.basicConfig(level=logging.DEBUG)
   elif options.verbose:
@@ -1777,16 +1792,6 @@ def main(argv=None):
   if not change_class:
     parser.error('For unversioned directory, <files> is not optional.')
   logging.info('Found %d file(s).', len(files))
-
-  gerrit_obj = None
-  if options.gerrit_url and options.gerrit_fetch:
-    assert options.issue and options.patchset
-    gerrit_obj = GerritAccessor(urlparse.urlparse(options.gerrit_url).netloc)
-    options.author = gerrit_obj.GetChangeOwner(options.issue)
-    options.description = gerrit_obj.GetChangeDescription(options.issue,
-                                                          options.patchset)
-    logging.info('Got author: "%s"', options.author)
-    logging.info('Got description: """\n%s\n"""', options.description)
 
   try:
     with canned_check_filter(options.skip_canned):

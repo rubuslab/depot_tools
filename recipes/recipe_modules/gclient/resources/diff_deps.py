555efde9eba71e69e198c160211fd05a910185ce
Greg Guterman <guterman@google.com>
2020-07-07 22:16:25 -0700

    Move diff to gclient
    Change-Id: I9e33461ea4ae81dc056d229f8604943745e30389

---
+++ recipes/recipe_modules/gclient/resources/diff_deps.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import subprocess
+import sys
+
+if '@' in os.environ.get('GCLIENT_URL'):
+  ref = os.environ.get('GCLIENT_URL').split('@')[-1]
+else:
+  # TODO(guterman): when the ref isn't included, we can't tell what the default branch is
+  sys.exit(0)
+
+try:
+  diff = subprocess.check_output("git diff --cached --name-only %s" % ref, shell=True)
+except subprocess.CalledProcessError:
+  sys.exit(0)
+
+for line in diff.split('\n'):
+  print('%s/%s' % (os.environ.get('GCLIENT_DEP_PATH', ''),  line))

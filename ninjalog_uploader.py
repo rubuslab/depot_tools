3af0cdaf173f4d61d490b9f26715d498ea743a62
Bruce Dawson <brucedawson@chromium.org>
2020-08-03 17:36:44 -0700

    Fix reference to enable_js_type_check
    A TODO in ninjalog_uploader.py referred to the closure_compile gn arg
    which was recently renamed (as of crrev.com/c/2248564) to
    enable_js_type_check. This updates it (and nadds a missing space). This
    is purely a comment fix.
    
    Change-Id: I117fa7910663491fd7c6e017fe98e2a977a75f13

--- ninjalog_uploader.py
+++ ninjalog_uploader.py
@@ -29,7 +29,7 @@ import time
 import httplib2
 
 # These build configs affect build performance a lot.
-# TODO(tikuta): Add 'blink_symbol_level'and 'closure_compile'.
+# TODO(tikuta): Add 'blink_symbol_level' and 'enable_js_type_check'.
 WHITELISTED_CONFIGS = ('symbol_level', 'use_goma', 'is_debug',
                        'is_component_build', 'enable_nacl', 'host_os',
                        'host_cpu', 'target_os', 'target_cpu')

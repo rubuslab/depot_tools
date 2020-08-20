85a821a8be033777dbd441e2b73b8f6a78c75d10
Ben Pastene <bpastene@chromium.org>
2020-08-20 16:57:37 -0700

    gclient: Preserve ANSI color codes when calling hooks.
    Still a WIP.
    COMMIT=false
    
    Bug: 1034063
    Change-Id: I4150f66ef215ece06f4c32482dcd4ded14eb1bfe

--- gclient_utils.py
+++ gclient_utils.py
@@ -10,6 +10,8 @@ import codecs
 import collections
 import contextlib
 import datetime
+import errno
+import fcntl
 import functools
 import io
 import logging
@@ -17,6 +19,7 @@ import operator
 import os
 import pipes
 import platform
+import pty
 import re
 import stat
 import subprocess
@@ -585,9 +588,12 @@ def CheckCallAndFilter(args, print_stdout=False, filter_fn=None,
   sleep_interval = RETRY_INITIAL_SLEEP
   run_cwd = kwargs.get('cwd', os.getcwd())
   for attempt in range(RETRY_MAX + 1):
+    pty_parent, pty_child = pty.openpty()
+
     kid = subprocess2.Popen(
-        args, bufsize=0, stdout=subprocess2.PIPE, stderr=subprocess2.STDOUT,
+        args, bufsize=0, stdout=pty_child, stderr=subprocess2.STDOUT,
         **kwargs)
+    os.close(pty_child)
 
     GClientChildren.add(kid)
 
@@ -608,7 +614,13 @@ def CheckCallAndFilter(args, print_stdout=False, filter_fn=None,
     try:
       line_start = None
       while True:
-        in_byte = kid.stdout.read(1)
+        try:
+          in_byte = os.read(pty_parent, 1)
+        except IOError as e:
+          if e.errno == errno.EIO:
+            in_byte = None
+          else:
+            raise e
         is_newline = in_byte in (b'\n', b'\r')
         if not in_byte:
           break
@@ -629,8 +641,8 @@ def CheckCallAndFilter(args, print_stdout=False, filter_fn=None,
       if line_start is not None:
         filter_line(command_output, line_start)
 
+      os.close(pty_parent)
       rv = kid.wait()
-      kid.stdout.close()
 
       # Don't put this in a 'finally,' since the child may still run if we get
       # an exception.

eda799bc061c8bd112e3918a2238b2f37c9b3670
Michael Savigny <msavigny@google.com>
2020-12-17 16:42:04 -0500

    Add reproxy setup/teardown to autoninja.
    Adds reproxy setup and teardown to autoninja.  Since reproxy is intended
    to run for a single build (unlike the goma proxy), having setup and
    teardown happen as part of autoninja makes management of the proxy
    execution easier.
    
    To use this as it is currently implemented, set the RBE_BIN_DIR and
    RBE_CFG_DIR environment variables to point to the reclient binaries and
    reclient configuration files.  Note the reproxy.cfg file is NOT included
    in this change, and at the time of this CL you need to provide one
    yourself.
    
    Bug: 1149386
    Change-Id: I23601cc9b13193ac617ffc5963b9d443f6840d33

--- autoninja.py
+++ autoninja.py
@@ -27,6 +27,11 @@ import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
+# Currently get reclient binary and config dirs from env.  If set and using rbe,
+# then automatically call bootstrap to start reproxy.
+reclient_bin_dir = os.environ.get("RBE_BIN_DIR", "")
+reclient_cfg_dir = os.environ.get("RBE_CFG_DIR", "")
+
 # The -t tools are incompatible with -j
 t_specified = False
 j_specified = False
@@ -67,7 +72,8 @@ for index, arg in enumerate(input_args[1:]):
 # Strip -o/--offline so ninja doesn't see them.
 input_args = [ arg for arg in input_args if arg != '-o' and arg != '--offline']
 
-use_remote_build = False
+use_goma = False
+use_rbe = False
 
 # Attempt to auto-detect remote build acceleration. We support gn-based
 # builds, where we look for args.gn in the build tree, and cmake-based builds
@@ -83,9 +89,13 @@ if os.path.exists(os.path.join(output_dir, 'args.gn')):
       #
       # Anything after a comment is not consider a valid argument.
       line_without_comment = line.split('#')[0]
-      if re.search(r'(^|\s)(use_goma|use_rbe)\s*=\s*true($|\s)',
+      if re.search(r'(^|\s)(use_goma)\s*=\s*true($|\s)',
                    line_without_comment):
-        use_remote_build = True
+        use_goma = True
+        continue
+      if re.search(r'(^|\s)(use_rbe)\s*=\s*true($|\s)',
+                   line_without_comment):
+        use_rbe = True
         continue
 else:
   for relative_path in [
@@ -97,7 +107,7 @@ else:
       with open(path) as file_handle:
         for line in file_handle:
           if re.match(r'^\s*command\s*=\s*\S+gomacc', line):
-            use_remote_build = True
+            use_goma = True
             break
 
 # If GOMA_DISABLED is set to "true", "t", "yes", "y", or "1" (case-insensitive)
@@ -109,7 +119,7 @@ else:
 # autoninja uses an appropriate -j value in this situation.
 goma_disabled_env = os.environ.get('GOMA_DISABLED', '0').lower()
 if offline or goma_disabled_env in ['true', 't', 'yes', 'y', '1']:
-  use_remote_build = False
+  use_goma = False
 
 # Specify ninja.exe on Windows so that ninja.bat can call autoninja and not
 # be called back.
@@ -132,7 +142,7 @@ args = prefix_args + [ninja_exe_path] + input_args[1:]
 
 num_cores = psutil.cpu_count()
 if not j_specified and not t_specified:
-  if use_remote_build:
+  if use_goma or use_rbe:
     args.append('-j')
     core_multiplier = int(os.environ.get('NINJA_CORE_MULTIPLIER', '40'))
     j_value = num_cores * core_multiplier
@@ -165,6 +175,18 @@ for i in range(len(args)):
 
 if os.environ.get('NINJA_SUMMARIZE_BUILD', '0') == '1':
   args += ['-d', 'stats']
+
+# If using rbe and the necessary environment variables are set, also start
+# reproxy (via bootstrap) before running ninja.
+if not offline and use_rbe and reclient_bin_dir and reclient_cfg_dir:
+  setup_args = [
+    'RBE_cfg=' + reclient_cfg_dir + '/reproxy.cfg',
+    reclient_bin_dir + '/bootstrap',
+    '--re_proxy=' + reclient_bin_dir + '/reproxy']
+
+  teardown_args = [reclient_bin_dir + '/bootstrap', '--shutdown']
+
+  args = setup_args + ['&&'] + args + ['&&'] + teardown_args
 
 if offline and not sys.platform.startswith('win'):
   # Tell goma or reclient to do local compiles. On Windows these environment

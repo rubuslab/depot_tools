cab224e9072338c195a03ed888d9d2dd6d998342
Gavin Mak <gavinmak@google.com>
2021-07-07 21:17:39 +0000

    Add python3 support to git recipe module
    Bug: 1227140
    Change-Id: I71ad53a978ecb7040b2482b97ee23ed4c1c8f59a

--- recipes/recipe_modules/git/api.py
+++ recipes/recipe_modules/git/api.py
@@ -4,6 +4,12 @@
 
 import itertools
 import re
+import sys
+
+if sys.version_info.major < 3:
+  _INTEGER_TYPES = (int, long)
+else:
+  _INTEGER_TYPES = (int,)  # pragma: no cover
 
 from recipe_engine import recipe_api
 
@@ -59,7 +65,8 @@ class GitApi(recipe_api.RecipeApi):
     """
     if previous_result:
       assert isinstance(previous_result, dict)
-      assert all(isinstance(v, long) for v in previous_result.values())
+      assert all(
+          isinstance(v, _INTEGER_TYPES) for v in previous_result.values())
       assert 'size' in previous_result
       assert 'size-pack' in previous_result
 
@@ -75,7 +82,7 @@ class GitApi(recipe_api.RecipeApi):
       result = {}
       for line in step_result.stdout.splitlines():
         name, value = line.split(':', 1)
-        result[name] = long(value.strip())
+        result[name] = int(value.strip())
 
       def results_to_text(results):
         return ['  %s: %s' % (k, v) for k, v in results.items()]

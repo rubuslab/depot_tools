dc15a6a0216962bd985554c394944254e74f0d5d
Ben Pastene <bpastene@chromium.org>
2021-11-23 15:07:35 -0800

    White space change to gerrit_client.py
    Commit: false
    Change-Id: I0221ae640831b81786d7bf5939bb2638511b7040

--- gerrit_client.py
+++ gerrit_client.py
@@ -2,6 +2,7 @@
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+# test
 
 """Simple client for the Gerrit REST API.
 

c454051a60defbbe8fae2cd66b3d6f0328bcf647
James Lee <ljjlee@google.com>
2022-01-05 13:32:30 +0100

    Use pylint 2.7 for depot_tools
    This includes a few fixes for specific errors, and disables several new
    warnings introduced in this version, in order to allow for an incremental
    migration.
    
    Bug:1262286
    Change-Id: Ib34ffb708bd0308a9ffd07ba866e2bee230e2ba9

--- presubmit_canned_checks.py
+++ presubmit_canned_checks.py
@@ -1059,7 +1059,7 @@ def GetPylint(input_api,
   # Leave this unreachable code in here so users can make
   # a quick local edit to diagnose pylint issues more
   # easily.
-  if True:
+  if False:
     # pylint's cycle detection doesn't work in parallel, so spawn a second,
     # single-threaded job for just that check.
 
@@ -1073,7 +1073,7 @@ def GetPylint(input_api,
       return [ GetPylintCmd(files, [], True) ]
 
   else:
-    return map(lambda x: GetPylintCmd([x], [], 1), files)
+    return map(lambda x: GetPylintCmd([x], [], False), files)
 
 
 def RunPylint(input_api, *args, **kwargs):

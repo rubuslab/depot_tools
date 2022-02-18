ca9c2d396bd1e007874b0d0c99f58b2bd70e9b1b
Chris Blume <cblume@chromium.org>
2022-02-18 02:58:08 -0500

    Correct trailing path separator
    In [1], I landed a change that contained a bug. It meant to trim off a
    trailing path separator. Instead of trimming the end, it trimmed
    everything but the first char (since the path separator is 1 char long).
    
    This CL fixes that bug.
    
    [1] https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/3451982
    
    Change-Id: Ie4aebfc1042908437bd603da1c20681f19c50f73

--- win_toolchain/package_from_installed.py
+++ win_toolchain/package_from_installed.py
@@ -188,7 +188,9 @@ def BuildFileList(override_dir, include_arm):
 
   # Strip off a trailing slash if present
   if sdk_path.endswith(os.path.sep):
-    sdk_path = sdk_path[:len(os.path.sep)]
+    print(sdk_path)
+    sdk_path = sdk_path[:-len(os.path.sep)]
+    print(sdk_path)
 
   debuggers_path = os.path.join(sdk_path, 'Debuggers')
   if not os.path.exists(debuggers_path):

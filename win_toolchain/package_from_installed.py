80096c122d7326eda8dbd19b5499f5a2e9cf77f5
Chris Blume <cblume@chromium.org>
2022-03-08 13:10:32 -0500

    Only call GetVSPath() once
    Currently, GetVSPath() is called twice. The result if that call is used
    locally in both situations.
    
    This commit makes only one call, passing the result to where it is
    needed.
    
    Change-Id: I186bd7f0387091ad16c2791de99d1de8a89f266c

--- win_toolchain/package_from_installed.py
+++ win_toolchain/package_from_installed.py
@@ -97,7 +97,7 @@ def BuildRepackageFileList(src_dir):
   return result
 
 
-def BuildFileList(override_dir, include_arm):
+def BuildFileList(override_dir, include_arm, vs_path):
   result = []
 
   # Subset of VS corresponding roughly to VC.
@@ -148,8 +148,6 @@ def BuildFileList(override_dir, include_arm):
         ('VC/redist/MSVC/14.*.*/debug_nonredist/arm64/Microsoft.VC*.DebugCRT',
          'sysarm64'),
     ]
-
-  vs_path = GetVSPath()
 
   for path in paths:
     src = path[0] if isinstance(path, tuple) else path
@@ -500,12 +498,13 @@ def main():
     _win_version = options.winver
     global _vc_tools
     vs_path = GetVSPath()
+    print(vs_path)
     temp_tools_path = ExpandWildcards(vs_path, 'VC/Tools/MSVC/14.*.*')
     # Strip off the leading vs_path characters and switch back to / separators.
     _vc_tools = temp_tools_path[len(vs_path) + 1:].replace('\\', '/')
 
     print('Building file list for VS %s Windows %s...' % (_vs_version, _win_version))
-    files = BuildFileList(options.override_dir, options.arm)
+    files = BuildFileList(options.override_dir, options.arm, vs_path)
 
     AddEnvSetup(files, options.arm)
 

e1a3416d0d34535efd35d365054d053a22fe19c1
Josip Sokcevic <sokcevic@google.com>
2022-03-15 16:36:54 -0700

    Explicitly run everything with python3
    R=aravindvasudev@google.com, gavinmak@google.com
    
    Change-Id: Iaa5e960c6226dea3a0814c7cb778d952eafb1502

--- git_retry.py
+++ git_retry.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -95,7 +95,7 @@ class GitRetry(object):
 
   def __call__(self, *args):
     returncode = 0
-    for i in xrange(self.retry_count):
+    for i in range(self.retry_count):
       # If the previous run failed and a delay is configured, delay before the
       # next run.
       delay = self.computeDelay(i)

99745fa9d57193eea6df8a7fef9bb8c1def77301
Rupert Ben Wiser <bewise@chromium.org>
2022-06-14 13:31:56 +0000

    Add JSON formatting to git cl format
    We would like to be able to format
    JSON files as part of git cl format.
    
    There are a lot of JSON files so
    we will add an allowed list to limit needing
    to format everything in one go.
    
    The formatting rule is very simple, just
    use python's default JSON dumps output
    and add an indentation of 2 spaces.
    
    Starting with the webview tools directory.
    
    Bug: chromium:1335324
    Change-Id: I7f3fcaff71f56fcc81459106772ec325553e3433

--- tests/git_cl_test.py
+++ tests/git_cl_test.py
@@ -4115,9 +4115,15 @@ class CMDFormatTestCase(unittest.TestCase):
     shutil.rmtree(self._top_dir)
     super(CMDFormatTestCase, self).tearDown()
 
+  def _make_temp_dir(self, fpath):
+    gclient_utils.safe_makedirs(os.path.join(self._top_dir, fpath))
+
   def _make_temp_file(self, fname, contents):
     gclient_utils.FileWrite(os.path.join(self._top_dir, fname),
                             ('\n'.join(contents)))
+
+  def _read_temp_file(self, fname):
+    return gclient_utils.FileRead(os.path.join(self._top_dir, fname))
 
   def _make_yapfignore(self, contents):
     self._make_temp_file('.yapfignore', contents)
@@ -4150,6 +4156,25 @@ class CMDFormatTestCase(unittest.TestCase):
     return_value = git_cl._RunClangFormatDiff(mock_opts, diff_file,
                                               self._top_dir, 'HEAD')
     self.assertEqual(0, return_value)
+
+  def testJSONformatDiff(self):
+    test_dir = os.path.join('android_webview', 'tools')
+    test_file = os.path.join(test_dir, 'test.json')
+    self._make_temp_dir(test_dir)
+    self._make_temp_file(test_file, ['{', '    "key": "too many spaces"', '}'])
+    diff_file = [os.path.join(self._top_dir, test_file)]
+    mock_opts = mock.Mock(dry_run=True)
+
+    # Dry run
+    return_value = git_cl._RunJSONFmt(mock_opts, diff_file)
+    self.assertEqual(2, return_value)
+
+    # Update files
+    mock_opts = mock.Mock(dry_run=False)
+    return_value = git_cl._RunJSONFmt(mock_opts, diff_file)
+    self.assertEqual(0, return_value)
+    self.assertEqual('\n'.join(["{", '  "key": "too many spaces"', '}\n']),
+                     self._read_temp_file(test_file))
 
   def testClangFormatDiff(self):
     git_cl.settings.GetFormatFullByDefault.return_value = False

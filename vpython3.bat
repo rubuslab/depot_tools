4faa2d21557d571ab16ee190465eb7dd474f848a
Brian Ryner <bryner@google.com>
2022-07-14 12:23:03 +1000

    Use the bundled interpreter instead of passing -vpython-interpreter.
    Once this is rolled out successfully, we will remove the depot_tools
    bootstrapping of python(3).
    
    Bug: 1318617
    Change-Id: If11b81f19d1d15eda74aae94ddc95c2997148c8e

--- vpython3.bat
+++ vpython3.bat
@@ -4,7 +4,6 @@
 :: found in the LICENSE file.
 
 setlocal
-for /f %%i in (%~dp0python3_bin_reldir.txt) do set PYTHON3_BIN_RELDIR=%%i
 
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
-"%~dp0\.cipd_bin\vpython3.exe" -vpython-interpreter "%~dp0\%PYTHON3_BIN_RELDIR%\python3.exe" %*
+"%~dp0\.cipd_bin\vpython3.exe" %*

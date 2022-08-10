efd1b81e0da73b34587d6108a5c05082dd84402c
Sylvain Defresne <sdefresne@chromium.org>
2022-08-10 13:41:38 +0200

    Update the copyright notice regexp in presubmit_canned_checks.py
    The Chromium script tools/boilerplate.py was changed to not use
    "All rights reserved." in the copyright notice when creating new
    files upon request from OSS licensing.
    
    Fix the regexp used in presubmit_canned_checks.py to make the
    "All rights reserved." part optional to avoid PRESUBMIT.py from
    warning about incorrect copyright notice for all files created
    using this tool.
    
    See https://crrev.com/c/3808222 for the change to the script.
    
    Bug: none
    Change-Id: I28f88a66d1b462a3834471cc5062573558b135f8

--- presubmit_canned_checks.py
+++ presubmit_canned_checks.py
@@ -654,7 +654,7 @@ def CheckLicense(input_api, output_api, license_re=None, project_name=None,
     # The (c) is deprecated, but tolerate it until it's removed from all files.
     license_re = (
         r'.*? Copyright (\(c\) )?%(year)s The %(project)s Authors\. '
-          r'All rights reserved\.\r?\n'
+          r'(?:All rights reserved\.)?\r?\n'
         r'.*? %(key_line)s\r?\n'
         r'.*? found in the LICENSE file\.(?: \*/)?\r?\n'
     ) % {

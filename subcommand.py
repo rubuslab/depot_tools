a8785135dd7b15dd82738c960faca38e918a3194
Arthur Milchior <arthurmilchior@google.com>
2022-08-11 15:00:50 +0200

    [depot_tools] improve error message on wrong command
    Tested:
    * `git cl ulpoad`
    * `git cl ulpoad -d`
    
    In both case, error message is improved.
    
    I’ll admit however that there is way to have the most important part
    "ulpoad is not a valid command" more proeminent.
    
    Bug: 1352093
    Change-Id: I4e72fe53b308ecd39a68839e9249958fef6ae4fa

--- subcommand.py
+++ subcommand.py
@@ -41,6 +41,7 @@ Explanation:
 """
 
 import difflib
+from logging.config import valid_ident
 import sys
 import textwrap
 
@@ -71,6 +72,7 @@ def CMDhelp(parser, args):
   if not any(i in ('-h', '--help') for i in args):
     args = args + ['--help']
   parser.parse_args(args)
+  print("CMDHelp")
   # Never gets there.
   assert False
 
@@ -178,10 +180,18 @@ class CommandDispatcher(object):
             '  %s%-*s%s %s\n' % (green, length, cmd_name, reset, doc)
             for cmd_name, doc in docs))
 
-  def _add_command_usage(self, parser, command):
+  def _add_command_usage(self, parser, command, original_command):
     """Modifies an OptionParser object with the function's documentation."""
     cmd_name = _function_to_name(command.__name__)
-    if cmd_name == 'help':
+    if (original_command is not None and 
+        original_command not in self.enumerate_commands()
+      ):
+      # There was an original command and it's not a valid one.
+      print("`%s` is not a valid `git cl` command name. \n\n" %
+            original_command)
+      parser.description = self._gen_commands_list()
+      cmd_name = '<command>'
+    elif cmd_name == 'help':
       cmd_name = '<command>'
       # Use the module's docstring as the description for the 'help' command if
       # available.
@@ -233,11 +243,13 @@ class CommandDispatcher(object):
     parser.format_description = lambda _: parser.description or ''
     parser.format_epilog = lambda _: parser.epilog or ''
 
+    original_command = None
     if args:
       if args[0] in ('-h', '--help') and len(args) > 1:
         # Reverse the argument order so 'tool --help cmd' is rewritten to
         # 'tool cmd --help'.
         args = [args[1], args[0]] + args[2:]
+      original_command = args[0]
       command = self.find_nearest_command(args[0])
       if command:
         if command.__name__ == 'CMDhelp' and len(args) > 1:
@@ -248,13 +260,13 @@ class CommandDispatcher(object):
           command = self.find_nearest_command(args[0]) or command
 
         # "fix" the usage and the description now that we know the subcommand.
-        self._add_command_usage(parser, command)
+        self._add_command_usage(parser, command, original_command)
         return command(parser, args[1:])
 
     cmdhelp = self.enumerate_commands().get('help')
     if cmdhelp:
       # Not a known command. Default to help.
-      self._add_command_usage(parser, cmdhelp)
+      self._add_command_usage(parser, cmdhelp, original_command)
       return cmdhelp(parser, args)
 
     # Nothing can be done.

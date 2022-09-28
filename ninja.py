abec632bec1d6b6e9e0e70cfdcd74333ea8bda5e
Junji Watanabe <jwata@google.com>
2022-09-28 14:59:56 +0900

    Add ninja wrapper to find CIPD ninja under buildtools
    > led get-builder "luci.webrtc.ci:Android32 (M Nexus5X)" |
      led edict-cr-cl https://webrtc-review.googlesource.com/c/src/+/275060/|
      led edit-recipe-bundle -O depot_tools=$HOME/depot_tools |
      led launch
    ...
    https://ci.chromium.org/swarming/task/5d9b8194b6745910?server=chromium-swarm.appspot.com
    
    Bug: 1340825
    Change-Id: I15ac3db6549162181e38c5ee41e83aed8dc06608

---
+++ ninja.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This script is a wrapper around the ninja binary that is pulled to
+third_party as part of gclient sync. It will automatically find the ninja
+binary when run inside a gclient source tree, so users can just type
+"ninja" or "ninja.exe" on the command line."""
+
+import gclient_paths
+import os
+import subprocess
+import sys
+
+
+DEPOT_TOOLS_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def fallbackToLegacyNinja(ninja_args):
+  print('depot_tools/ninja.py: Fallback to a deprecated legacy ninja binary. '
+        'Please note that this binary will be removed soon.', file=sys.stderr)
+
+  exe_name = ''
+  if sys.platform == 'linux':
+    exe_name = 'ninja-linux64'
+  elif sys.platform == 'darwin':
+    exe_name = 'ninja-mac'
+  elif sys.platform == 'win32':
+    exe_name = 'ninja-legacy.exe'
+  ninja_path = os.path.join(DEPOT_TOOLS_ROOT, exe_name)
+  return subprocess.call([ninja_path] + ninja_args)
+
+
+def main(args):
+  # Get gclient root + src.
+  primary_solution_path = gclient_paths.GetPrimarySolutionPath()
+  if not primary_solution_path:
+    print(
+      'depot_tools/ninja.py: Could not find checkout in any parent of the '
+      'current path.\nThis must be run inside a checkout.', file=sys.stderr)
+    return 1
+  ninja_path = os.path.join(primary_solution_path, 'third_party',
+                         'ninja', 'ninja' + gclient_paths.GetExeSuffix())
+  if not os.path.exists(ninja_path):
+    print('depot_tools/ninja.py: Could not find ninja executable at: %s'
+          % ninja_path, file=sys.stderr)
+    return fallbackToLegacyNinja(args[1:])
+
+  return subprocess.call([ninja_path] + args[1:])
+
+
+if __name__ == '__main__':
+  try:
+    sys.exit(main(sys.argv))
+  except KeyboardInterrupt:
+    sys.stderr.write('interrupted\n')
+    sys.exit(1)

35f5eb11849bd0de5de8fd785588e3cf2c67a615
Gavin Mak <gavinmak@google.com>
2022-10-18 23:28:35 +0000

    test
    Change-Id: If0be46fc9f4425f5d58655c20099d5d9817b8551

--- tests/bot_update_coverage_test.py
+++ tests/bot_update_coverage_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env vpython3
 # Copyright (c) 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# found in the LICENSE file. chromium.git.corp.google.com
 
 import codecs
 import copy

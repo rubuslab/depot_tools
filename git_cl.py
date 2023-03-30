076ef953957e4283e09d8659e8d032e81d1be92c
Arthur Milchior <arthurmilchior@chromium.org>
2023-03-30 17:38:15 +0200

    [depot tools] Extract IssueReverse
    This method being distinct from the remaining of Issue number: None (None), it seems cleaner to extract it from the remaining of the codebase.
    Also, in the next CL, it’ll be used by .
    
    Bug: 1428476
    Change-Id: I8b36736da63a45d80ca97d947f88f3ae1c5515c1

--- git_cl.py
+++ git_cl.py
@@ -4240,6 +4240,27 @@ def IssueBranchMap():
   return issue_branch_map
 
 
+def IssueReverse(options, args):
+  # Reverse issue lookup.
+  issue_branch_map = IssueBranchMap()
+
+  if not args:
+    args = sorted(issue_branch_map.keys())
+  result = {}
+  for issue in args:
+    try:
+      issue_num = int(issue)
+    except ValueError:
+      print('ERROR cannot parse issue number: %s' % issue, file=sys.stderr)
+      continue
+    result[issue_num] = issue_branch_map.get(issue_num)
+    print('Branch for issue number %s: %s' %
+          (issue, ', '.join(issue_branch_map.get(issue_num) or ('None', ))))
+  if options.json:
+    write_json(options.json, result)
+  return 0
+
+
 @subcommand.usage('[issue_number]')
 @metrics.collector.collect_metrics('git cl issue')
 def CMDissue(parser, args):
@@ -4256,24 +4277,7 @@ def CMDissue(parser, args):
   options, args = parser.parse_args(args)
 
   if options.reverse:
-    # Reverse issue lookup.
-    issue_branch_map = IssueBranchMap()
-
-    if not args:
-      args = sorted(issue_branch_map.keys())
-    result = {}
-    for issue in args:
-      try:
-        issue_num = int(issue)
-      except ValueError:
-        print('ERROR cannot parse issue number: %s' % issue, file=sys.stderr)
-        continue
-      result[issue_num] = issue_branch_map.get(issue_num)
-      print('Branch for issue number %s: %s' % (
-          issue, ', '.join(issue_branch_map.get(issue_num) or ('None',))))
-    if options.json:
-      write_json(options.json, result)
-    return 0
+    return IssueReverse(options, args)
 
   if len(args) > 0:
     issue = ParseIssueNumberArgument(args[0])

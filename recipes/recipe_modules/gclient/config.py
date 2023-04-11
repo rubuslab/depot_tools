9dab691f8e7c8ed4b935bdb5f0631e7b40e07780
Joanna Wang <jojwang@google.com>
2023-04-11 11:16:38 -0400

    Update infra contexts to use infra_superproject.
    Bug: 1415507
    Change-Id: I92a60640679115ae188c6de610ef695876a536b5

--- recipes/recipe_modules/gclient/config.py
+++ recipes/recipe_modules/gclient/config.py
@@ -284,14 +284,12 @@ def expect_tests(c):
   soln.url = 'https://chromium.googlesource.com/infra/testing/expect_tests.git'
   c.got_revision_mapping['expect_tests'] = 'got_revision'
 
-
 @config_ctx()
 def infra_superproject(c):
   soln = c.solutions.add()
   soln.name = '.'
   soln.url = 'https://chromium.googlesource.com/infra/infra_superproject.git'
-  # TODO(crbug.com/1415507): When builders can checkout internal code, add
-  # soln.custom_vars = {'checkout_internal': True}
+  c.got_revision_mapping['infra'] = 'got_revision'
   c.repo_path_map.update({
       'https://chromium.googlesource.com/infra/luci/gae':
       ('infra/go/src/go.chromium.org/gae', 'HEAD'),
@@ -302,6 +300,15 @@ def infra_superproject(c):
       'https://chromium.googlesource.com/infra/luci/recipes-py':
       ('infra/recipes-py', 'HEAD')
   })
+
+
+@config_ctx()
+def infra_internal_superproject(c):
+  soln = c.solutions.add()
+  soln.name = '.'
+  soln.custom_vars = {'checkout_internal': True}
+  soln.url = 'https://chromium.googlesource.com/infra/infra_superproject.git'
+  c.got_revision_mapping['infra_internal'] = 'got_revision'
 
 
 @config_ctx()

4e06004fbf204bc6dc1612f3e32f03e0b7946d1c
Junji Watanabe <jwata@google.com>
2023-04-19 11:03:39 +0900

    [siso] add autosiso wrappers
    Developers will be able to use `autosiso` command to invoke siso builds.
    This CL extracts reclient management logic from `ninja_reclient` to be  reused in both Ninja and Siso builds.
    
    Bug: b/278675516
    Change-Id: I3e64a3188db184f4d3f851063a0feef7d3a73d6d

--- autosiso.py
+++ autosiso.py
@@ -2,22 +2,26 @@
 # Copyright 2023 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""This script is a wrapper around the ninja.py script that also
-handles the client lifecycle safely. It will automatically start
-reproxy before running ninja and stop reproxy when ninja stops
-for any reason eg. build completes, keyboard interupt etc."""
+"""
+Developers invoke this script via autosiso or autosiso.bat to simply run
+Siso builds.
+"""
 
+import os
+import subprocess
 import sys
 
-import ninja
 import reclient_helper
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def main(argv):
   with reclient_helper.build_context(argv) as ret_code:
     if ret_code:
       return ret_code
-    return ninja.main(argv)
+    siso_path = os.path.join(SCRIPT_DIR, 'siso.py')
+    return subprocess.call([sys.executable, siso_path, 'ninja'] + argv[1:])
 
 
 if __name__ == '__main__':

b872ef3fe320c1ebc3c8dbac1d9765f70d5cf83d
randymaldonado <randymaldonado@google.com>
2023-06-21 18:50:20 +0000

    [depot_tools] updating gclient config for luci_py and luci_go to pull from infra_superproject
    Recipe-Nontrivial-Roll: build
    Recipe-Nontrivial-Roll: infra
    
    Change-Id: I0a5591dc470feffe3469da6b4fd9a300a8b8007e

--- recipes/recipe_modules/gclient/config.py
+++ recipes/recipe_modules/gclient/config.py
@@ -357,7 +357,7 @@ def luci_gae(c):
   del m['infra']
   m['infra/go/src/go.chromium.org/gae'] = 'got_revision'
 
-@config_ctx(includes=['infra'])
+@config_ctx(includes=['infra_superproject'])
 def luci_go(c):
   # luci-go is checked out as a part of infra.git solution at HEAD.
   c.revisions['infra'] = 'refs/heads/main'
@@ -367,7 +367,7 @@ def luci_go(c):
   del m['infra']
   m['infra/go/src/go.chromium.org/luci'] = 'got_revision'
 
-@config_ctx(includes=['infra'])
+@config_ctx(includes=['infra_superproject'])
 def luci_py(c):
   # luci-py is checked out as part of infra just to have appengine
   # pre-installed, as that's what luci-py PRESUBMIT relies on.

bb63f5763e8a95849057479b9ca83bc61dfd89e8
Joanna Wang <jojwang@google.com>
2023-08-08 23:42:19 +0000

    gclient: add gitmodules command to update/add submodules based on DEPS.
    Bug: 1421776
    Change-Id: Id1ac48c4b65c17027fa24d0ba350a1a7f2eec64d

--- tests/gclient_git_smoketest.py
+++ tests/gclient_git_smoketest.py
@@ -32,6 +32,46 @@ class GClientSmokeGIT(gclient_smoketest_base.GClientSmokeBase):
     if not self.enabled:
       self.skipTest('git fake repos not available')
 
+  def testGitmodules_relative(self):
+    self.gclient(['config', self.git_base + 'repo_19', '--name', 'dir'],
+                 cwd=self.git_base)
+    self.gclient(['sync'], cwd=self.git_base)
+    self.gclient(['gitmodules'], cwd=self.git_base + 'dir')
+
+    gitmodules = os.path.join(self.git_base, 'dir', '.gitmodules')
+    with open(gitmodules) as f:
+      contents = f.read().splitlines()
+      self.assertEqual([
+          '[submodule "some_repo"]', '\tpath = some_repo', '\turl = /repo_2',
+          '\tgclient-condition = not foo_checkout',
+          '[submodule "chicken/dickens"]', '\tpath = chicken/dickens',
+          '\turl = /repo_3'
+      ], contents)
+
+  def testGitmodules_not_relative(self):
+    self.gclient(['config', self.git_base + 'repo_20', '--name', 'foo'],
+                 cwd=self.git_base)
+    self.gclient(['sync'], cwd=self.git_base)
+    self.gclient(['gitmodules'], cwd=self.git_base + 'foo')
+
+    gitmodules = os.path.join(self.git_base, 'foo', '.gitmodules')
+    with open(gitmodules) as f:
+      contents = f.read().splitlines()
+      self.assertEqual([
+          '[submodule "some_repo"]', '\tpath = some_repo', '\turl = /repo_2',
+          '\tgclient-condition = not foo_checkout',
+          '[submodule "chicken/dickens"]', '\tpath = chicken/dickens',
+          '\turl = /repo_3'
+      ], contents)
+
+  def testGitmodules_not_in_gclient(self):
+    self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'],
+                 cwd=self.git_base)
+    self.gclient(['sync'], cwd=self.git_base)
+
+    with self.assertRaisesRegex(AssertionError, 'from a gclient workspace'):
+      self.gclient(['gitmodules'], cwd=self.root_dir)
+
   def testSync(self):
     self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'])
     # Test unversioned checkout.

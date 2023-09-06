d5273627a9ea3d2b26db9127a244a55b3d823e98
Gavin Mak <gavinmak@google.com>
2023-09-06 04:02:47 +0000

    Remove last py2 workarounds
    python3 is the only supported version of python in depot_tools.
    
    Changes include:
      * updating python version requirements
      * removing third_party/six
    
    Bug: 1475402
    Change-Id: Ib86231413fcac494ceb27d1783e53b9d86533c26

--- gclient.py
+++ gclient.py
@@ -3721,7 +3721,7 @@ def path_contains_tilde():
 
 
 def can_run_gclient_and_helpers():
-  if sys.hexversion < 0x02060000:
+  if sys.version_info[0] < 3:
     print(
         '\nYour python version %s is unsupported, please upgrade.\n' %
         sys.version.split(' ', 1)[0],

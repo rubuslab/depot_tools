73ed03e0f57a223d245a53adf3cda1c3e362b004
Philipp Wollermann <philwo@google.com>
2023-09-21 20:27:37 +0900

    Improve autoninja, autosiso scripts.
    autoninja now correctly handles all combinations of
    {Ninja, Siso} x {local, Goma, reclient} by either invoking them if
    that's a supported config, or printing a helpful error message if not
    (e.g. Siso with Goma).
    
    Move all logic from the shell scripts and .bat wrappers into Python.
    
    This turns auto{ninja,siso}{,.bat} into trivial wrappers that just
    call autoninja.py and contain no other logic, which reduces duplication.
    
    Merge autosiso.py into autoninja.py to prepare removal of autosiso.
    
    Print a deprecation warning from the autosiso wrappers for the same
    reason.
    
    Reduce duplicated parsing of Ninja args where possible and instead
    directly pass the required information.
    
    Address old TODO about wrong quoting by adopting the same quoting
    mechanism used by mb.py. This ensures that printed command-lines
    are all always copy & pasteable into the terminal.
    
    Bug: b/293657720
    Change-Id: I7ee2edd3be4370ab997777809db90c7d5e6ea400

--- autosiso.bat
+++ autosiso.bat
@@ -2,13 +2,27 @@
 :: Copyright 2023 The Chromium Authors
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
+
 setlocal
+
+set scriptdir=%~dp0
+
+echo FYI: autosiso has been merged into autoninja, which will now
+echo      automatically delegate to the right tool depending on your
+echo      GN args. Please run autoninja going forward.
 
 :: Ensure that "depot_tools" is somewhere in PATH so this tool can be used
 :: standalone, but allow other PATH manipulations to take priority.
 set PATH=%PATH%;%~dp0
 
-:: Defer control.
-:: Add double quotes to the arguments to preserve the special '^' character.
-:: See autosiso.py for more information.
-python3 "%~dp0\autosiso.py" "%*"
+if "%*" == "/?" (
+  rem Handle "autosiso /?" which will otherwise give help on the "call" command
+  @call %scriptdir%python-bin\python3.bat %scriptdir%autoninja.py --help
+  exit /b
+)
+
+:: Don't use vpython - it is too slow to start.
+:: Don't use python3 because it doesn't work in git bash on Windows and we
+:: should be consistent between autoninja.bat and the autoninja script used by
+:: git bash.
+@call %scriptdir%python-bin\python3.bat %scriptdir%autoninja.py "%*"

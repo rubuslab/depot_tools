047a657231660dd87b296546ef98d965699ceb84
Philipp Wollermann <philwo@google.com>
2023-10-11 15:57:03 +0900

    Execute the ninja command from autoninja.py, instead of printing it.
    Part of my bigger auto{ninja,siso} refactoring.
    
    Tested:
    - Handling of the ^^ suffix on Windows still works correctly.
    - Handling of error codes - i.e.; making sure
      "autoninja base_unittests && base_unittests.exe" behaves properly
      in the success/failure case.
    - Make sure the command prompt title is reliably reset on exit.
    
    R=brucedawson@chromium.org, jwata@google.com, tikuta@chromium.org
    
    Bug: b/293657720
    Change-Id: I275a775fdc5abb6555f79d4beab76cd0914d4bd6

--- tests/autoninja_test.py
+++ tests/autoninja_test.py
@@ -6,9 +6,11 @@
 import multiprocessing
 import os
 import os.path
+import io
 import sys
 import unittest
-import unittest.mock
+import contextlib
+from unittest import mock
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, ROOT_DIR)
@@ -41,9 +43,9 @@ class AutoninjaTest(trial_dir.TestCase):
         autoninja.main([])
 
     def test_autoninja_goma(self):
-        with unittest.mock.patch(
+        with mock.patch(
                 'subprocess.call',
-                return_value=0) as mock_call, unittest.mock.patch.dict(
+                return_value=0) as mock_call, mock.patch.dict(
                     os.environ,
                     {"GOMA_DIR": os.path.join(self.root_dir, 'goma_dir')}):
             out_dir = os.path.join('out', 'dir')
@@ -52,7 +54,7 @@ class AutoninjaTest(trial_dir.TestCase):
                 os.path.join(
                     'goma_dir', 'gomacc.exe'
                     if sys.platform.startswith('win') else 'gomacc'), 'content')
-            args = autoninja.main(['autoninja.py', '-C', out_dir]).split()
+            args = autoninja.main(['autoninja.py', '-C', out_dir])
             mock_call.assert_called_once()
 
         self.assertIn('-j', args)
@@ -67,7 +69,7 @@ class AutoninjaTest(trial_dir.TestCase):
               'RBE_v=2')
         write(os.path.join('buildtools', 'reclient', 'version.txt'), '0.0')
 
-        args = autoninja.main(['autoninja.py', '-C', out_dir]).split()
+        args = autoninja.main(['autoninja.py', '-C', out_dir])
 
         self.assertIn('-j', args)
         parallel_j = int(args[args.index('-j') + 1])
@@ -91,6 +93,34 @@ class AutoninjaTest(trial_dir.TestCase):
         self.assertListEqual(lines, [
             'use_remoteexec=true',
         ])
+
+    @mock.patch('sys.platform', 'win32')
+    def test_print_cmd_windows(self):
+        args = [
+            'C:\\Program Files\\Python 3\\bin\\python3.exe', 'ninja.py', '-C',
+            'out\\direc tory\\',
+            '../../base/types/expected_macros_unittest.cc^', '-j', '140'
+        ]
+        with contextlib.redirect_stderr(io.StringIO()) as f:
+            autoninja._print_cmd(args)
+            self.assertEqual(
+                f.getvalue(),
+                '"C:\\Program Files\\Python 3\\bin\\python3.exe" ninja.py -C ' +
+                '"out\\direc tory\\" ' +
+                '../../base/types/expected_macros_unittest.cc^^ -j 140\n')
+
+    @mock.patch('sys.platform', 'linux')
+    def test_print_cmd_linux(self):
+        args = [
+            '/home/user name/bin/python3', 'ninja.py', '-C', 'out/direc tory/',
+            '../../base/types/expected_macros_unittest.cc^', '-j', '140'
+        ]
+        with contextlib.redirect_stderr(io.StringIO()) as f:
+            autoninja._print_cmd(args)
+            self.assertEqual(
+                f.getvalue(),
+                "'/home/user name/bin/python3' ninja.py -C 'out/direc tory/' " +
+                "'../../base/types/expected_macros_unittest.cc^' -j 140\n")
 
 
 if __name__ == '__main__':

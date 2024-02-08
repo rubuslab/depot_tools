a572b55134f5c648f80afb68f9a70e17989fd97c
Dustin J. Mitchell <djmitche@chromium.org>
2024-02-08 20:23:24 +0000

    Use --null with 'git config --list'
    This fixes issues parsing configs containing newlines
    
    Bug: 324449654
    Change-Id: Ie39877d3beb6a3eb990e68dc0eac0701ff804922

--- scm.py
+++ scm.py
@@ -121,15 +121,15 @@ class GIT(object):
         """
         if cwd not in GIT._CONFIG_CACHE:
             try:
-                rawConfig = GIT.Capture(['config', '--list'],
+                rawConfig = GIT.Capture(['config', '--list', '--null'],
                                         cwd=cwd,
                                         strip_out=False)
             except subprocess2.CalledProcessError:
                 return {}
 
             cfg = defaultdict(list)
-            for line in rawConfig.splitlines():
-                key, value = map(str.strip, line.split('=', 1))
+            for line in rawConfig.split(b'\0'):
+                key, value = map(str.strip, line.split(b'\n', 1))
                 cfg[key].append(value)
 
             GIT._CONFIG_CACHE[cwd] = cfg

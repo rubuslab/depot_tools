385da0d93fb4d3827d2d04892b399b4eaee02518
Stephanie Kim <kimstephanie@google.com>
2024-03-13 16:34:14 +0000

    Add is_first_class_gcs migration file
    If is_first_class_gcs file exists with contents '1'
    - GCS hooks will download content and then overwrite the file with '0'
    - First class GCS deps will download as necessary by checking the hash
    file.
    
    If there is no is_first_class_gcs file or its content is '0' for a GCS hook:
    - Hook will run normally and, if content is downloaded, an is_first_class_gcs
    file will be written with '0'
    
    If there is no is_first_class_gcs file or its content is '0' for a first
    class gcs dep:
    - Content will be downloaded and is_first_class_gcs will be set to '1'
    
    Verified locally with migrating and unmigrating a GCS hook.
    
    Bug: b/324418194
    Change-Id: Ida8541cb70839b86e99115bcabfc38bc87e40136

--- tests/gclient_gcs_smoketest.py
+++ tests/gclient_gcs_smoketest.py
@@ -39,12 +39,16 @@ class GClientSmokeGcs(gclient_smoketest_base.GClientSmokeBase):
             'abcd123\n',
             'src/another_gcs_dep/llvmfile.tar.gz':
             'tarfile',
+            'src/another_gcs_dep/is_first_class_gcs':
+            '1\n',
             'src/another_gcs_dep/extracted_dir/extracted_file':
             'extracted text',
             'src/gcs_dep/deadbeef':
             'tarfile',
             'src/gcs_dep/hash':
             'abcd123\n',
+            'src/gcs_dep/is_first_class_gcs':
+            '1\n',
             'src/gcs_dep/extracted_dir/extracted_file':
             'extracted text',
         })
@@ -73,6 +77,7 @@ class GClientSmokeGcs(gclient_smoketest_base.GClientSmokeBase):
         tree.update({
             'src/repo12/extracted_dir/extracted_file': 'extracted text',
             'src/repo12/hash': 'abcd123\n',
+            'src/repo12/is_first_class_gcs': '1\n',
             'src/repo12/path_to_file.tar.gz': 'tarfile',
         })
         self.assertTree(tree)
@@ -90,6 +95,7 @@ class GClientSmokeGcs(gclient_smoketest_base.GClientSmokeBase):
         tree.update({
             'src/repo12/extracted_dir/extracted_file': 'extracted text',
             'src/repo12/hash': 'abcd123\n',
+            'src/repo12/is_first_class_gcs': '1\n',
             'src/repo12/path_to_file.tar.gz': 'tarfile',
         })
         self.assertTree(tree)
@@ -105,6 +111,7 @@ class GClientSmokeGcs(gclient_smoketest_base.GClientSmokeBase):
         tree.update({
             'src/repo12/extracted_dir/extracted_file': 'extracted text',
             'src/repo12/hash': 'abcd123\n',
+            'src/repo12/is_first_class_gcs': '1\n',
             'src/repo12/path_to_file.tar.gz': 'tarfile',
         })
         self.assertTree(tree)

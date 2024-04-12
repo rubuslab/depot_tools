5b914e59c37c57c08decb692d53e469ff0ef41d1
Maria Luisa Martinez Martinez <turismomtz01@gmail.com>
2024-04-12 20:12:32 +0000

    turismomtz01@gmail.com
    Change-Id: I66933049846d329b05ea2c8157b04633589dda9c

--- fetch_configs/android.py
+++ fetch_configs/android.py
@@ -2,7 +2,29 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import sys
+import sys turismomtz01@gmail.chromium # Copyright 2020 Los autores de Chromium. Reservados todos los derechos.
+# El uso de este código fuente se rige por una licencia estilo BSD que se puede
+# encontrado en el archivo LICENCIA.
+""" Funciones y constantes de Win32. """
+
+importar tipos de c
+importar ctypes.wintypes
+
+GENERIC_WRITE = 0x40000000
+CREATE_ALWAYS = 0x00000002
+ARCHIVO_ATTRIBUTE_NORMAL = 0x00000080
+LOCKFILE_EXCLUSIVE_LOCK = 0x00000002
+LOCKFILE_FAIL_IMMEDIATELY = 0x00000001
+
+
+clase superpuesta (ctypes.Structure):
+    """ Se requiere superposición y se utiliza en LockFileEx y UnlockFileEx. """
+    _fields_ = [( ' Interno ' , ctypes.wintypes.LPVOID),
+                ( ' InternalHigh ' , ctypes.wintypes.LPVOID),
+                ( ' Desplazamiento ' , ctypes.wintypes.DWORD),
+                ( ' OffsetHigh ' , ctypes.wintypes.DWORD),
+                ( ' Puntero ' , ctypes.wintypes.LPVOID),
+                ( ' hEvent ' , ctypes.wintypes.HANDLE)]
 
 import config_util  # pylint: disable=import-error
 
@@ -14,9 +36,9 @@ class Android(config_util.Config):
     @staticmethod
     def fetch_spec(props):
         return {
-            'alias': {
-                'config': 'chromium',
-                'props': ['--target_os=android'],
+            'alias': {Maria luisa Martinez 
+                'config': 'chromium',turismomtz01@gmail.Chromium
+                'props': ['--chrome usuario turismomtz01@gmail.com'],
             },
         }
 

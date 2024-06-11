af97defe5814520502806a07955c26436d464590
Richard Wang <richardwa@google.com>
2024-06-11 14:55:06 +0900

    Use $HOME/.config/depot_tools on linux for .cfg files
    Or $XDG_CONFIG_HOME/.config/depot_tools if set
    
    A followup CL can add support for win/macos.
    
    Bug: b/345092320
    Change-Id: I877baa4d7fd912b42cfcd88ad0aa347b700a89f2

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -5,7 +5,9 @@
 
 import logging
 import os
+import shutil
 import sys
+import tempfile
 import unittest
 from unittest import mock
 
@@ -39,6 +41,86 @@ class GitCacheTest(unittest.TestCase):
         mock_getmtime.side_effect = Exception
         version = utils.depot_tools_version()
         self.assertEqual(version, 'unknown')
+
+
+class ConfigDirTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testWin(self):
+        sys.platform = 'win'
+
+        self.assertEqual(DEPOT_TOOLS_ROOT, utils.depot_tools_config_dir())
+
+    def testMac(self):
+        sys.platform = 'darwin'
+
+        self.assertEqual(DEPOT_TOOLS_ROOT, utils.depot_tools_config_dir())
+
+    def testOther(self):
+        sys.platform = 'foo'
+
+        self.assertEqual(DEPOT_TOOLS_ROOT, utils.depot_tools_config_dir())
+
+    def testLinuxDefault(self):
+        os.environ = {}
+        sys.platform = 'linux'
+
+        self.assertEqual(
+            os.path.join(os.path.expanduser('~/.config'), 'depot_tools'),
+            utils.depot_tools_config_dir())
+
+    def testLinuxCustom(self):
+        os.environ = {'XDG_CONFIG_HOME': '/my/home'}
+        sys.platform = 'linux'
+
+        self.assertEqual('/my/home/depot_tools', utils.depot_tools_config_dir())
+
+
+class ConfigPathTest(unittest.TestCase):
+
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp(prefix='utils_test')
+        self.config_dir = os.path.join(self.temp_dir, 'test_files')
+
+        self.move = mock.Mock()
+        self.isfile = mock.Mock()
+
+        mock.patch('os.path.isfile', self.isfile).start()
+        mock.patch('shutil.move', self.move).start()
+        mock.patch('utils.depot_tools_config_dir',
+                   lambda: self.config_dir).start()
+
+        self.addCleanup(mock.patch.stopall)
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+    def testCreatesConfigDir(self):
+
+        def EnsureConfigPathExists(path):
+            return path == os.path.join(self.config_dir, 'metrics.cfg')
+
+        self.isfile.side_effect = EnsureConfigPathExists
+
+        self.assertEqual(os.path.join(self.config_dir, 'metrics.cfg'),
+                         utils.depot_tools_config_path('metrics.cfg'))
+        self.assertTrue(os.path.exists(self.config_dir))
+        self.move.assert_not_called()
+
+    def testMigratesLegacy(self):
+
+        def EnsureLegacyPathExists(path):
+            return path == os.path.join(DEPOT_TOOLS_ROOT, 'metrics.cfg')
+
+        self.isfile.side_effect = EnsureLegacyPathExists
+
+        self.assertEqual(os.path.join(self.config_dir, 'metrics.cfg'),
+                         utils.depot_tools_config_path('metrics.cfg'))
+        self.move.assert_called_once_with(
+            os.path.join(DEPOT_TOOLS_ROOT, 'metrics.cfg'),
+            os.path.join(self.config_dir, 'metrics.cfg'))
 
 
 if __name__ == '__main__':

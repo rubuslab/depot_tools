c4ccbd251673edbc4d43ed076bd153934970bf8c
Allen Li <ayatane@chromium.org>
2024-07-11 14:11:24 -0700

    [git_auth] Add apply_global tests
    Bug: b/351024645
    Change-Id: I54e55545c27275165b3435707e2b47095c8fcac9

--- tests/git_auth_test.py
+++ tests/git_auth_test.py
@@ -20,7 +20,8 @@ import scm_mock
 class TestConfigChanger(unittest.TestCase):
 
     def setUp(self):
-        scm_mock.GIT(self)
+        self.global_state = {}
+        scm_mock.GIT(self, global_state=self.global_state)
 
     def test_apply_new_auth(self):
         git_auth.ConfigChanger(
@@ -136,6 +137,31 @@ class TestConfigChanger(unittest.TestCase):
             '/some/fake/dir': {},
         }
         self.assertEqual(scm.GIT._dump_config_state(), want)
+
+    def test_apply_global_new_auth(self):
+        git_auth.ConfigChanger(
+            mode=git_auth.ConfigMode.NEW_AUTH,
+            remote_url=
+            'https://chromium.googlesource.com/chromium/tools/depot_tools.git',
+        ).apply_global('/some/fake/dir')
+        want = {
+            'credential.https://chromium.googlesource.com/.helper':
+            ['', 'luci'],
+        }
+        self.assertEqual(self.global_state, want)
+
+    def test_apply_global_new_auth_sso(self):
+        git_auth.ConfigChanger(
+            mode=git_auth.ConfigMode.NEW_AUTH_SSO,
+            remote_url=
+            'https://chromium.googlesource.com/chromium/tools/depot_tools.git',
+        ).apply_global('/some/fake/dir')
+        want = {
+            'protocol.sso.allow': ['always'],
+            'url.sso://chromium/.insteadOf':
+            ['https://chromium.googlesource.com/'],
+        }
+        self.assertEqual(self.global_state, want)
 
 
 if __name__ == '__main__':

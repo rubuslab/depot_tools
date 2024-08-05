585927ad3bad98b8d1c48fceffe5daab7c7671c4
Gavin Mak <gavinmak@google.com>
2024-08-05 17:56:50 +0000

    Add gerrit-cp command for cherry picking a chain of CLs via Gerrit
    Users who want to upload multiple cherry picks usually run "git
    cherry-pick" locally, multiple times. Gerrit does not recognize
    these changes as cherry picks and neither do other services that
    query cherry pick info from Gerrit, e.g. rubber stamper.
    
    For Gerrit to identify a change as a true cherry pick, you need to
    use their Cherry Pick Revision REST API endpoint. gerrit-cp uses it
    to create a chain of cherry pick CLs recognized by Gerrit.
    
    Bug: b/341792235
    Change-Id: I4ba75da3901f6ea68c1debd65820e802da681798

--- git_cl.py
+++ git_cl.py
@@ -4503,6 +4503,78 @@ def CMDissue(parser, args):
     return 0
 
 
+def _create_commit_message(orig_message, bug=None):
+    """Returns a commit message for the cherry picked CL."""
+    orig_message_lines = orig_message.splitlines()
+    subj_line = orig_message_lines[0]
+    new_message = (f'Cherry pick "{subj_line}"\n\n'
+                   "Original change's description:\n")
+    for line in orig_message_lines:
+        new_message += f'> {line}\n'
+    if bug:
+        new_message += f'\nBug: {bug}\n'
+    return new_message
+
+
+# TODO(b/341792235): Add metrics.
+@subcommand.usage('[revisions ...]')
+def CMDcherry_pick(parser, args):
+    """Upload a chain of cherry picks to Gerrit."""
+    if gclient_utils.IsEnvCog():
+        print('cherry-pick command is not supported in non-git environment',
+              file=sys.stderr)
+        return 1
+
+    parser.add_option('--host',
+                      help='Gerrit host, e.g. chromium-review.googlesource.com')
+    parser.add_option('--branch', help='Gerrit branch, e.g. refs/heads/main')
+    parser.add_option('--bug',
+                      help='Bug to add to the description of each change.')
+    options, args = parser.parse_args(args)
+
+    if not options.host or not options.branch:
+        parser.error('Both host and branch are required.')
+    if not args:
+        parser.error('No revisions to cherry pick.')
+
+    # Gerrit needs a change ID for each commit we cherry pick.
+    change_ids_to_message = {}
+    for commit in args:
+        message = git_common.run('show', '-s', '--format=%B', commit).strip()
+        if change_id := git_footers.get_footer_change_id(message):
+            change_ids_to_message[change_id[0]] = message
+            continue
+        raise RuntimeError(f'Change ID not found for {commit}')
+
+    print(f'Creating chain of {len(change_ids_to_message)} cherry pick(s)...')
+
+    # Gerrit only supports cherry picking one commit per change, so we have
+    # to cherry pick each commit individually and create a chain of CLs.
+    parent_change_num = None
+    for change_id, orig_message in change_ids_to_message.items():
+        message = _create_commit_message(orig_message, options.bug)
+
+        # Create a cherry pick first, then rebase. If we create a chained CL
+        # then cherry pick, the change will lose its relation to the parent.
+        new_change_info = gerrit_util.CherryPick(options.host,
+                                                 change_id,
+                                                 options.branch,
+                                                 message=message)
+        new_change_id = new_change_info['change_id']
+        new_change_num = new_change_info['_number']
+
+        if parent_change_num:
+            gerrit_util.RebaseChange(options.host, new_change_id,
+                                     parent_change_num)
+        parent_change_num = new_change_num
+
+        orig_subj_line = orig_message.splitlines()[0]
+        print(f'Created cherry pick of "{orig_subj_line}": '
+              f'https://{options.host}/q/{new_change_id}')
+
+    return 0
+
+
 @metrics.collector.collect_metrics('git cl comments')
 def CMDcomments(parser, args):
     """Shows or posts review comments for any changelist."""

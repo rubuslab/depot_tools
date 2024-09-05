0a470583fb0d6809e28885d294fc1dbcb0f999c0
Arthur Eubanks <aeubanks@google.com>
2024-09-05 20:35:32 +0000

    [git-cl] Only ignore deleted files for formatting purposes
    Multiple people have complained that they want to format files when changing class names.
    
    This CL essentially reverts https://crrev.com/c/4904205, meaning we will again run clang-format on files that were copied/moved.
    
    https://crrev.com/c/5199134 is probably the proper fix by only looking at lines that were actually changed, but does not yet work with non-C++ formatters.
    
    Bug: 737568
    Change-Id: Ie12c802316d5e293f2946c532e1600c342caaf59

--- git_cl.py
+++ git_cl.py
@@ -6884,7 +6884,7 @@ def CMDformat(parser, args):
                      'Are you in detached state?')
 
     # Filter out copied/renamed/deleted files
-    diff_output = RunGitDiffCmd(['--name-only', '--diff-filter=crd'],
+    diff_output = RunGitDiffCmd(['--name-only', '--diff-filter=d'],
                                 upstream_commit, files)
     diff_files = diff_output.splitlines()
 
